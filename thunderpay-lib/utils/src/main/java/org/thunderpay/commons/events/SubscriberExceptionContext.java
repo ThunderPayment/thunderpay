a3a0e5a1bd95c7a7ab657c44d380181cc7a667ca
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-14 15:29:32 +0530

    thunderpay-lib: events[SubscriberExceptionContext]

--- thunderpay-lib/utils/src/main/java/org/thunderpay/commons/events/SubscriberExceptionContext.java
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/events/SubscriberExceptionContext.java
@@ -12,10 +12,37 @@
 package org.thunderpay.commons.events;
 
 import java.lang.reflect.Method;
+import org.thunderpay.commons.utils.Preconditions;
 
 public class SubscriberExceptionContext {
+
     private final EventBus eventBus;
     private final Object event;
     private final Object subscriber;
     private final Method subscriberMethod;
+
+
+    SubscriberExceptionContext(final EventBus eventBus, final Object event, final Object subscriber, final Method subscriberMethod) {
+        this.eventBus = Preconditions.checkNotNull(eventBus);
+        this.event = Preconditions.checkNotNull(event);
+        this.subscriber = Preconditions.checkNotNull(subscriber);
+        this.subscriberMethod = Preconditions.checkNotNull(subscriberMethod);
+    }
+
+
+    public EventBus getEventBus() {
+        return eventBus;
+    }
+
+    public Object getEvent() {
+        return event;
+    }
+
+    public Object getSubscriber() {
+        return subscriber;
+    }
+
+    Method getSubscriberMethod() {
+        return subscriberMethod;
+    }
 }
