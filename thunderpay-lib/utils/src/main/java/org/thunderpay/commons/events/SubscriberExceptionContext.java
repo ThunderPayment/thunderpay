337bc26e0b81f7cb93038b470d549fd2d39bcb8c
Williams <82570807+NukeWilliams@users.noreply.github.com>
2024-12-14 11:40:00 +0530

    Subscriber Exception Context

--- thunderpay-lib/utils/src/main/java/org/thunderpay/commons/events/SubscriberExceptionContext.java
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/events/SubscriberExceptionContext.java
@@ -8,3 +8,14 @@
  * @copyright Copyright (c) 2024 ThunderPayment Developers, NukeWiliams
  *
  */
+
+package org.thunderpay.commons.events;
+
+import java.lang.reflect.Method;
+
+public class SubscriberExceptionContext {
+    private final EventBus eventBus;
+    private final Object event;
+    private final Object subscriber;
+    private final Method subscriberMethod;
+}
