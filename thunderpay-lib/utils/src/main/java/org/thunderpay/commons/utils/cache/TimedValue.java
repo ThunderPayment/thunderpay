ad406a7a65e2c18af491ea667bf641ffb973914f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-14 16:52:32 +0530

    thunderpay-lib: cache[TimedValue]

---
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/utils/cache/TimedValue.java
@@ -0,0 +1,46 @@
+package org.thunderpay.commons.utils.cache;
+
+import org.thunderpay.commons.utils.Preconditions;
+import java.util.Objects;
+
+class TimedValue<V> {
+    private final long expireTime;
+    private final V value;
+
+    TimedValue(final long timeoutMillis, final V value) {
+        this.expireTime = System.currentTimeMillis() + timeoutMillis;
+        this.value = Preconditions.checkNotNull(value, "LOG: Timedvalue cannot be null");
+    }
+
+    boolean isTimeout() {
+        return System.currentTimeMillis() >= expireTime;
+    }
+
+    V getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "TimedValue {" + "expireTime=" + expireTime + ", value=" + value + "}";
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final TimedValue<?> that = (TimedValue<?>) o;
+        return expireTime == that.expireTime && value.equals(that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(expireTime, value);
+    }
+}
