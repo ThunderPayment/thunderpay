cd612732f62d45bc7299bd02624ecc1a1c8583f2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-15 19:01:53 +0530

    thunderpay-lib: utils[Joiner]

--- thunderpay-lib/utils/src/main/java/org/thunderpay/commons/utils/Joiner.java
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/utils/Joiner.java
@@ -18,18 +18,19 @@ import javax.annotation.CheckForNull;
 import static java.util.Objects.requireNonNull;
 
 public final class Joiner {
+
     public static Joiner on(final String separator) {
         return new Joiner(separator);
     }
 
-    public static Joiner on(final char seperator) {
-        return new Joiner(String.valueOf(seperator));
+    public static Joiner on(final char separator) {
+        return new Joiner(String.valueOf(separator));
     }
 
-    private final String seperator;
+    private final String separator;
 
-    private Joiner(final String seperator) {
-        this.seperator = Preconditions.checkNotNull(seperator);
+    private Joiner(final String separator) {
+        this.separator = Preconditions.checkNotNull(separator);
     }
 
     public String join(final Iterable<?> parts) {
@@ -47,5 +48,49 @@ public final class Joiner {
             throw new AssertionError(impossible);
         }
         return builder;
+    }
+
+    public <A extends Appendable> A appendTo(final A appendable, final Iterator<?> parts) throws IOException {
+        Preconditions.checkNotNull(appendable);
+        if (parts.hasNext()) {
+            appendable.append(toString(parts.next()));
+            while (parts.hasNext()) {
+                appendable.append(separator);
+                appendable.append(toString(parts.next()));
+            }
+        }
+        return appendable;
+    }
+
+    CharSequence toString(final Object part) {
+        requireNonNull(part);
+        return (part instanceof CharSequence) ? (CharSequence) part : part.toString();
+    }
+
+    public String join(@CheckForNull final Object first, @CheckForNull final Object second, final Object... rest) {
+        return join(iterable(first, second, rest));
+    }
+
+    private static Iterable<Object> iterable(final Object first, final Object second, final Object[] rest) {
+        Preconditions.checkNotNull(rest);
+        return new AbstractList<Object>() {
+            @Override
+            public int size() {
+                return rest.length + 2;
+            }
+
+            @Override
+            @CheckForNull
+            public Object get(final int index) {
+                switch (index) {
+                    case 0:
+                        return first;
+                    case 1:
+                        return second;
+                    default:
+                        return rest[index - 2];
+                }
+            }
+        };
     }
 }
