dc4ff8c85c60a40ade5ff10b88293a31d127cd2d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-15 12:42:41 +0530

    thunderpay-lib: events[EventBusException]

--- thunderpay-lib/utils/src/main/java/org/thunderpay/commons/events/EventBusException.java
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/events/EventBusException.java
@@ -1,3 +1,14 @@
+/**
+ * @file EventBusException
+ * @author Krisna Pranav
+ * @brief Event Bus Exception Handlers + Contexts.
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
 package org.thunderpay.commons.events;
 
 public class EventBusException extends Exception {
