fc8e139a5bbd3d4fb4420dec5ea06bb3d1e70f7a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-15 12:42:49 +0530

    thunderpay-lib: events[LoggingHandler]

--- thunderpay-lib/utils/src/main/java/org/thunderpay/commons/events/LoggingHandler.java
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/events/LoggingHandler.java
@@ -1,3 +1,14 @@
+/**
+ * @file LoggingHandler.java
+ * @author Krisna Pranav
+ * @brief Log Handler
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
 package org.thunderpay.commons.events;
 
 import java.lang.reflect.Method;
