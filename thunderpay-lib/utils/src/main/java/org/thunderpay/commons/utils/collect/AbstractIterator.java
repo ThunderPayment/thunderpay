1f62f1bfba14654240b1d676a731943df11f23c3
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-15 12:56:27 +0530

    thunderpay-lib: collect[AbstractIterator]

---
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/utils/collect/AbstractIterator.java
@@ -0,0 +1,69 @@
+/**
+ * @file AbstractIterator
+ * @author Krisna Pranav
+ * @brief Abstract Iterator
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+
+package org.thunderpay.commons.utils.collect;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import javax.annotation.CheckForNull;
+import org.thunderpay.commons.utils.Preconditions;
+
+public abstract class AbstractIterator<T> implements Iterator<T> {
+    private State state = State.NOT_READY;
+
+    protected AbstractIterator() {}
+
+    private enum State
+    {
+        READY,
+        NOT_READY,
+        DONE,
+        FAILED,
+    }
+
+    @CheckForNull
+    private T next;
+
+    @CheckForNull
+    protected abstract T computeNext();
+
+    @CheckForNull
+    protected final T endOfData() {
+        state = State.DONE;
+        return null;
+    }
+
+    @Override
+    public final boolean hasNext() {
+        Preconditions.checkState(state != State.FAILED, "AbstractIterator failed");
+        switch (state) {
+            case DONE:
+                return false:
+            case READY:
+                return true;
+            default:
+        }
+
+        return tryToComputeNext();
+    }
+
+    private boolean tryToComputeNext() {
+        state = State.FAILED;
+        next = computeNext();
+        if (state != State.DONE) {
+            state = State.READY;
+            return true;
+        }
+
+        return false;
+    }
+}
