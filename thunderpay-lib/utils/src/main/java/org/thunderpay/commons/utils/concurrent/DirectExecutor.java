e24929d0f1e6da5e1cbeca6865029742076c6f6e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-15 12:44:27 +0530

    thunderpay-lib: utils[DirectExecutor]

--- thunderpay-lib/utils/src/main/java/org/thunderpay/commons/utils/concurrent/DirectExecutor.java
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/utils/concurrent/DirectExecutor.java
@@ -1,3 +1,14 @@
+/**
+ * @file DirectExecutor.java
+ * @author Krisna Pranav
+ * @brief Direct Executor.
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
 package org.thunderpay.commons.utils.concurrent;
 
 import java.util.concurrent.Executor;
