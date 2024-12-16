a33a6f3a955edf4ed238a5f5434ae1f0de30f0c9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-16 16:34:12 +0530

    thunderpay-lib: metric-api[Counting]

---
+++ thunderpay-lib/metric-api/src/main/java/org/thunderpay/metric/api/Counting.java
@@ -0,0 +1,17 @@
+/**
+ * @file Counting.java
+ * @author Krisna Pranav
+ * @brief Counting
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+
+package org.thunderpay.metric.api;
+
+public interface Counting {
+    long getCount();
+}
