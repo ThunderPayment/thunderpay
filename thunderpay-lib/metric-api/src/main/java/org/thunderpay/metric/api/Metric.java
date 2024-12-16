37e80357dbad241e7d8d93f337a7ccc7b8694e2d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-16 16:34:03 +0530

    thunderpay-lib: metric-api[Metric]

---
+++ thunderpay-lib/metric-api/src/main/java/org/thunderpay/metric/api/Metric.java
@@ -0,0 +1,15 @@
+/**
+ * @file Metric.java
+ * @author Krisna Pranav
+ * @brief Metric
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.metric.api;
+
+public interface Metric {
+}
