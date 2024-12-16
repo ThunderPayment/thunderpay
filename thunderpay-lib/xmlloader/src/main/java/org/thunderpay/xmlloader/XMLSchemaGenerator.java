a34a0e759293529b9cd6172412a249f76d3269f8
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-16 11:53:38 +0530

    thunderpay-lib: xmlloader[XMLSchemaGenerator]

--- thunderpay-lib/xmlloader/src/main/java/org/thunderpay/xmlloader/XMLSchemaGenerator.java
+++ thunderpay-lib/xmlloader/src/main/java/org/thunderpay/xmlloader/XMLSchemaGenerator.java
@@ -37,27 +37,70 @@ import org.w3c.dom.Document;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.SchemaOutputResolver;
 
 public class XMLSchemaGenerator {
     private static final int MAX_SCHEMA_SIZE_IN_BYTES = 100000;
 
-    public static void main(final String[] args) throws IOException {
+    public static void main(final String[] args) throws IOException, TransformerException, JAXBException, ClassNotFoundException {
         if (args.length != 2) {
             printUsage();
             System.exit(0);
         }
-
         final Class<?> clazz = ClassLoader.getSystemClassLoader().loadClass(args[1]);
+
         final JAXBContext context = JAXBContext.newInstance(clazz);
         String xsdFileName = "Schema.xsd";
         xsdFileName = args[0] + "/" + xsdFileName;
         final FileOutputStream s = new FileOutputStream(xsdFileName);
-
         pojoToXSD(context, s);
     }
 
     private static void printUsage() {
         System.out.println(XMLSchemaGenerator.class.getName() + " <file> <class1>");
+
     }
-    
+
+    public static String xmlSchemaAsString(final Class<?> clazz) throws IOException, TransformerException, JAXBException {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream(MAX_SCHEMA_SIZE_IN_BYTES);
+        final JAXBContext context = JAXBContext.newInstance(clazz);
+        pojoToXSD(context, output);
+        return new String(output.toByteArray(), UTF_8);
+    }
+
+    public static InputStream xmlSchema(final Class<?> clazz) throws IOException, TransformerException, JAXBException {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream(MAX_SCHEMA_SIZE_IN_BYTES);
+        final JAXBContext context = JAXBContext.newInstance(clazz);
+        pojoToXSD(context, output);
+        return new ByteArrayInputStream(output.toByteArray());
+    }
+
+    public static void pojoToXSD(final JAXBContext context, final OutputStream out)
+            throws IOException, TransformerException {
+        final List<DOMResult> results = new ArrayList<DOMResult>();
+
+        context.generateSchema(new SchemaOutputResolver() {
+            @Override
+            public Result createOutput(final String ns, final String file)
+                    throws IOException {
+                final DOMResult result = new DOMResult();
+                result.setSystemId(file);
+                results.add(result);
+                return result;
+            }
+        });
+
+        final DOMResult domResult = results.get(0);
+        final Document doc = (Document) domResult.getNode();
+
+        final TransformerFactory tFactory = TransformerFactory.newInstance();
+        final Transformer transformer = tFactory.newTransformer();
+
+        final DOMSource source = new DOMSource(doc);
+        final StreamResult result = new StreamResult(out);
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.transform(source, result);
+    }
+
 }
