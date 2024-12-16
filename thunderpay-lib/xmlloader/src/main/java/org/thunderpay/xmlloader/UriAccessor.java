b9bf341a2aa5c36546afcecec4621d8ead16876b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-16 11:55:06 +0530

    thunderpay-lib: xmlloader[UriAccessor]

---
+++ thunderpay-lib/xmlloader/src/main/java/org/thunderpay/xmlloader/UriAccessor.java
@@ -0,0 +1,29 @@
+/**
+ * @file UriAccessor.java
+ * @author Krisna Pranav
+ * @brief URI Accessor
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.xmlloader;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.JarURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Scanner;
+import org.thunderpay.commons.utils.io.Resources;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class UriAccessor {
+    private static final String URI_SCHEME_FOR_ARCHIVE_FILE = "jar:file";
+    private static final String URI_SCHEME_FOR_CLASSPATH = "jar";
+    private static final String URI_SCHEME_FOR_FILE = "file";
+}
