845443f868dcff5e35aeeec19b9a009999440cd3
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-16 16:15:20 +0530

    thunderpay-lib: impl[HealthyResult]

---
+++ thunderpay-lib/metric-api/src/main/java/org/thunderpay/health/impl/HealthyResult.java
@@ -0,0 +1,51 @@
+/**
+ * @file HealthyResult
+ * @author Krisna Pranav
+ * @brief Healthy Result
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+
+package org.thunderpay.health.impl;
+
+import org.thunderpay.health.api.Result;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HealthyResult implements Result {
+    private final String message;
+    private final long time;
+    private final Map<String, Object> details;
+
+    public HealthyResult(final String message, final long time, final Map<String, Object> details) {
+        this.message = message;
+        this.time = time;
+        this.details = details == null ? Collections.emptyMap() : new HashMap<>(details);
+    }
+
+    @Override
+    public boolean isHealthy() {
+        return true;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public Throwable getError() {
+        return null;
+    }
+
+    @Override
+    public long getTime() {
+        return time;
+    }
+}
