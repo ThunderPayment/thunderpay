526466aa863a6371088423cc3e3b3cdc0eee56e5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-16 12:02:26 +0530

    thunderpay-lib: xmlloader[UriAccessor]

--- thunderpay-lib/xmlloader/src/main/java/org/thunderpay/xmlloader/UriAccessor.java
+++ thunderpay-lib/xmlloader/src/main/java/org/thunderpay/xmlloader/UriAccessor.java
@@ -23,6 +23,7 @@ import org.thunderpay.commons.utils.io.Resources;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class UriAccessor {
+
     private static final String URI_SCHEME_FOR_ARCHIVE_FILE = "jar:file";
     private static final String URI_SCHEME_FOR_CLASSPATH = "jar";
     private static final String URI_SCHEME_FOR_FILE = "file";
@@ -33,16 +34,51 @@ public class UriAccessor {
 
     public static URL toURL(final URI inputURI) throws IOException, URISyntaxException {
         final String scheme = inputURI.getScheme();
+
         final URI uri;
-
         if (scheme == null) {
-            
-        } else if (scheme.equals(URI_SCHEME_FOR_FILE)) {
-
+            uri = new URI(Resources.getResource(inputURI.toString()).toExternalForm());
+        } else if (scheme.equals(URI_SCHEME_FOR_FILE) &&
+                   !inputURI.getSchemeSpecificPart().startsWith("/")) { // interpret URIs of this form as relative path uris
+            uri = new File(inputURI.getSchemeSpecificPart()).toURI();
         } else {
             uri = inputURI;
         }
-        
+
         return uri.toURL();
+    }
+
+    public static InputStream accessUri(final String uri) throws IOException, URISyntaxException {
+        return accessUri(new URI(uri));
+    }
+
+    public static InputStream accessUri(final URI uri) throws IOException, URISyntaxException {
+        final String scheme = uri.getScheme();
+        if (URI_SCHEME_FOR_CLASSPATH.equals(scheme)) {
+            if (uri.toString().startsWith(URI_SCHEME_FOR_ARCHIVE_FILE)) {
+                return getInputStreamFromJarFile(uri);
+            } else {
+                return UriAccessor.class.getResourceAsStream(uri.getPath());
+            }
+        }
+
+        final URL url = toURL(uri);
+        return url.openConnection().getInputStream();
+    }
+
+    private static InputStream getInputStreamFromJarFile(final URI uri) throws IOException {
+        final URL url = uri.toURL();
+        final JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+
+        return jarURLConnection.getJarFile().getInputStream(jarURLConnection.getJarEntry());
+    }
+
+    public static String accessUriAsString(final String uri) throws IOException, URISyntaxException {
+        return accessUriAsString(new URI(uri));
+    }
+
+    public static String accessUriAsString(final URI uri) throws IOException, URISyntaxException {
+        final InputStream stream = accessUri(uri);
+        return new Scanner(stream, UTF_8.name()).useDelimiter("\\A").next();
     }
 }
