be88550346915296f6175d51f7e8ef55cc1df949
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-20 17:15:28 +0530

    thunderpay-lib: metric-api[Metered]

---
+++ thunderpay-lib/metric-api/src/main/java/org/thunderpay/metric/api/annotation/Metered.java
@@ -0,0 +1,30 @@
+/**
+ * @file Metered.java
+ * @author Krisna Pranav
+ * @brief Metered
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.metric.api.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Inherited
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+public @interface Metered {
+    String name() default "";
+
+    boolean absolute() default false;
+}
