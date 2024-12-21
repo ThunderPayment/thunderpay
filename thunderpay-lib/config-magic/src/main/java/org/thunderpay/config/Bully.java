46055b7c1278c3e3a0b571dd590d0fcf06ef1cac
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-21 16:44:26 +0530

    thunderpay-lib: config-magic[bully]

--- thunderpay-lib/config-magic/src/main/java/org/thunderpay/config/Bully.java
+++ thunderpay-lib/config-magic/src/main/java/org/thunderpay/config/Bully.java
@@ -11,11 +11,11 @@
 
 package org.thunderpay.config;
 
-import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.WildcardType;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -62,5 +62,84 @@ class Bully {
         coercibles.addAll(TYPE_COERCIBLES);
     }
 
-    
+    public void addCoercible(final Coercible<?> coercible) {
+        coercibles.add(coercible);
+        mappings.clear();
+    }
+
+    public synchronized Object coerce(final Type type, final String value, final Separator separator) {
+        if (type instanceof Class) {
+            final Class<?> clazz = (Class<?>) type;
+
+            if (clazz.isArray()) {
+            }
+        }
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private Object coerceCollection(final Class<?> containerType, final Class<?> elemType, final String value, final Separator separator) {
+        if (value == null) {
+            return null;
+        } else {
+            Collection result = null;
+
+            if (Set.class.equals(containerType)) {
+                result = new LinkedHashSet();
+            } else if (Collection.class.equals(containerType) || List.class.equals(containerType)) {
+                result = new ArrayList();
+            } else if (Collection.class.isAssignableFrom(containerType)) {
+                try {
+                    final Constructor<?> ctor = containerType.getConstructor();
+
+                    if (ctor != null) {
+                        result = (Collection) ctor.newInstance();
+                    }
+                } catch (final Exception ex) {}
+            }
+
+            if (result == null) {
+                throw new IllegalStateException(String.format("%s", containerType, value));
+            }
+
+            if (value.length() > 0) {
+                for (final String token : value.split(separator == null ? Separator.DEFAULT : separator.value())) {
+                    result.add(coerce(elemType, token));
+                }
+            }
+
+            return result;
+        }
+    }
+
+    private Object coerce(final Class<?> clazz, final String value) {
+        Coercer<?> coercer = getCoercerFor(coercibles, clazz);
+
+        if (coercer == null) {
+            coercer = getCoercerFor(DEFAULT_COERCIBLES, clazz);
+
+            if (coercer == null) {
+                throw new IllegalStateException(String.format("%s", clazz, value));
+            }
+        }
+
+        return coercer.coerce(value);
+    }
+
+    private Coercer<?> getCoercerFor(final List<Coercible<?>> coercibles, final Class<?> type) {
+        Coercer<?> typeCoercer = mappings.get(type);
+
+        if(typeCoercer == null) {
+            for (final Coercible<?> coercible : coercibles) {
+                final Coercer<?> coercer = coercible.accept(type);
+
+                if (coercer != null) {
+                    mappings.put(type, coercer);
+                    typeCoercer = coercer;
+                    break;
+                }
+            }
+        }
+
+        return typeCoercer;
+    }
 }
