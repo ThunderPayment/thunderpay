b6c47c1404f1904a65137b607d7f55c69ef70d64
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-21 16:32:05 +0530

    thunderpay-lib: config-magic[Default]

---
+++ thunderpay-lib/config-magic/src/main/java/org/thunderpay/config/Default.java
@@ -0,0 +1,23 @@
+/**
+ * @file Default.java
+ * @author Krisna Pranav
+ * @brief Default
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Default {
+    String value();
+}
