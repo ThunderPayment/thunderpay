ff11cac1e19e6721cdd9d5c6649f722ab9a81677
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-21 16:34:28 +0530

    thunderpay-lib: config-magic[Description]

---
+++ thunderpay-lib/config-magic/src/main/java/org/thunderpay/config/Description.java
@@ -0,0 +1,23 @@
+/**
+ * @file Description
+ * @author Krisna Pranav
+ * @brief Description
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Description {
+    String value() default ":nodoc:";
+}
