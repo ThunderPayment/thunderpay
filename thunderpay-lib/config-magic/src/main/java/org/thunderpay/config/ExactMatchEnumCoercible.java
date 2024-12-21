0dd89fe6b5de04042a8bc4ea604509f95551357e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-21 11:46:36 +0530

    thunderpay-lib: config-magic[ExcatMatchEnumCoercible]

---
+++ thunderpay-lib/config-magic/src/main/java/org/thunderpay/config/ExactMatchEnumCoercible.java
@@ -0,0 +1,41 @@
+/**
+ * @file ExactMatchEnumCoercible.java
+ * @author Krisna Pranav
+ * @brief Exact Match Enum Coercible
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.config;
+
+import java.lang.reflect.Method;
+
+public class ExactMatchEnumCoercible implements Coercible<Object> {
+    public Coercible<Object> accept(final Class<?> clazz) {
+        if (!clazz.isEnum()) {
+            return null;
+        }
+
+        try {
+            final Method m = clazz.getMethod("valueOf", String.class);
+            return new Coercer<Object>() {
+                public Object coerce(final String value) {
+                    if (value == null) {
+                        return null;
+                    }
+
+                    try {
+                        return m.invoke(null, value);
+                    } catch (final Exception e) {
+                        throw DefaultCoercibles.convertException(e);
+                    }
+                }
+            };
+        } catch (final NoSuchMethodException e) {
+            throw new IllegalStateException("<EnumType>.valueOf(String) missing!", e);
+        }
+    }
+}
