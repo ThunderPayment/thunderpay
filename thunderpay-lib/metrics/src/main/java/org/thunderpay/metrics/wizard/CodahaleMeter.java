7eaa06a35bde643ee850725efdd4a73fa287cdbc
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-23 19:18:26 +0530

    thunderpay-lib: metrics[wizard(CodahaleMeter)]

---
+++ thunderpay-lib/metrics/src/main/java/org/thunderpay/metrics/wizard/CodahaleMeter.java
@@ -0,0 +1,33 @@
+/**
+ * @file CodahaleMeter.java
+ * @author Krisna Pranav
+ * @brief Codahale Meter
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.metrics.wizard;
+
+import org.thunderpay.metric.api.Meter;
+import com.codahale.metrics.Metered;
+
+public class CodahaleMeter implements Metered {
+    private final Meter meter;
+
+    public CodahaleMeter(final Meter meter) {
+        this.meter = meter;
+    }
+
+    @Override
+    public long getCount() {
+        return meter.getCount();
+    }
+
+    @Override
+    public double getFifteenMinuteRate() {
+        return meter.getFifteenMinuteRate();
+    }
+}
