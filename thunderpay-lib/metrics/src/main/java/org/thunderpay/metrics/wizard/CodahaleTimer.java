a04de1919dbe2cf32eaa059e8a729e182b34ed76
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-23 19:24:50 +0530

    thunderpay-lib: metric[wizard(CodahaleTimer)]

---
+++ thunderpay-lib/metrics/src/main/java/org/thunderpay/metrics/wizard/CodahaleTimer.java
@@ -0,0 +1,42 @@
+/**
+ * @file CodahaleTimer.java
+ * @author Krisna Pranav
+ * @brief Codahale Timer
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.metrics.wizard;
+
+
+import org.thunderpay.metric.api.Sampling;
+import org.thunderpay.metric.api.Timer;
+import com.codahale.metrics.Metered;
+import com.codahale.metrics.Snapshot;
+import com.codahale.metrics.UniformSnapshot;
+
+public class CodahaleTimer implements Metered, Sampling {
+    private final Timer timer;
+
+    public CodahaleTimer(final Timer timer) {
+        this.timer = timer;
+    }
+
+    @Override
+    public long getCount() {
+        return timer.getCount();
+    }
+
+    @Override
+    public double getFifteenMinuteRate() {
+        return timer.getFifteenMinuteRate();
+    }
+
+    @Override
+    public double getFiveMinuteRate() {
+        return timer.getFiveMinuteRate();
+    }
+}
