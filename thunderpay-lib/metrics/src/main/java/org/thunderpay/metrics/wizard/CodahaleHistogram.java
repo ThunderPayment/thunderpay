ca76ab3ff76baaf7d8980d59fae25d44e54c8eb1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-23 19:15:05 +0530

    thunderpay-lib: metrics[wizard(CodahaleHistogram)]

---
+++ thunderpay-lib/metrics/src/main/java/org/thunderpay/metrics/wizard/CodahaleHistogram.java
@@ -0,0 +1,32 @@
+/**
+ * @file CodahaleHistogram.java
+ * @author Krisna Pranav
+ * @brief Codahale Histogram
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.metrics.wizard;
+
+import org.thunderpay.metric.api.Counting;
+import org.thunderpay.metric.api.Histogram;
+import com.codahale.metrics.Metric;
+import com.codahale.metrics.Snapshot;
+import com.codahale.metrics.UniformSnapshot;
+import org.thunderpay.metric.api.Sampling;
+
+public class CodahaleHistogram implements Metric, Sampling, Counting {
+    private final Histogram histogram;
+
+    public CodahaleHistogram(final Histogram histogram) {
+        this.histogram = histogram;
+    }
+
+    @Override
+    public long getCount() {
+        return histogram.getCount();
+    }
+}
