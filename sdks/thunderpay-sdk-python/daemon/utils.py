5553003af7ec137c370e7436317829160089ca5a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-06 15:14:49 +0530

    sdks: thunderpay-sdk-python[daemon(utils)]

--- sdks/thunderpay-sdk-python/daemon/utils.py
+++ sdks/thunderpay-sdk-python/daemon/utils.py
@@ -24,3 +24,46 @@ from decimal import Decimal
 from typing import Any
 from urllib.parse import parse_qsl, urlencode, urlparse, urlunparse
 from aiohttp import web
+
+CONVERT_RATE = 100000000
+
+def noop_cast(v):
+    return v
+
+def format_satoshis(x):
+    return f"{(Decimal(x) / CONVERT_RATE):.08f}"
+
+def load_spec(spec_file, exit_on_error=True):
+    try:
+        with open(spec_file) as f:
+            return json.loads(f.read())
+    except (OSError, json.JSONDecodeError) as e:
+        if exit_on_error:
+            sys.exit(e)
+    
+    return {}
+
+def maybe_update_key(dest, other, key):
+    other_value = other.get(key, {})
+    
+    if key in dest:
+        dest[key].update(other_value)
+    else:
+        dest[key] = other_value
+    
+def rpc(f=None, requires_wallet=False, requires_network=False, requires_lightning=False):
+    def wrapper(f):
+        f.is_handler = True
+        f.requires_wallet = bool(requires_network)
+        f.requires_network = bool(requires_network)
+        f.requires_lightning = bool(requires_lightning)
+        return f
+
+    if f:
+        return wrapper(f)
+    
+    return wrapper
+
+
+
+        
