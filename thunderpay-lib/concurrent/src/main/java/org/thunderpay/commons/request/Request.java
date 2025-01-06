5c4359d3cfebf08b1a93a6faf632fd58c6791e7a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-06 11:13:58 +0530

    thunderpay-lib: concurrent[commons(Request)]

--- thunderpay-lib/concurrent/src/main/java/org/thunderpay/commons/request/Request.java
+++ thunderpay-lib/concurrent/src/main/java/org/thunderpay/commons/request/Request.java
@@ -14,5 +14,15 @@ package org.thunderpay.commons.request;
 public class Request {
     private static final ThreadLocal<RequestData> perThreadRequest = new ThreadLocal<RequestData>();
 
+    public static RequestData getPerThreadRequestData() {
+        return perThreadRequest.get();
+    }
 
+    public static void setPerThreadRequestData(final RequestData requestData) {
+        perThreadRequest.set(requestData);
+    }
+
+    public static void resetPerThreadRequestData() {
+        perThreadRequest.set(null);
+    }
 }
