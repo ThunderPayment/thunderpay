dea1f31daef767bc03b32b7874634c2efd9ac564
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-06 10:36:42 +0530

    thunderpay-lib: clock[Clock(basic clock funcs)]

---
+++ thunderpay-lib/clock/src/main/java/org/thunderpay/clock/Clock.java
@@ -0,0 +1,32 @@
+/**
+ * @file AllowConcurrentEvents.java
+ * @author Krisna Pranav
+ * @brief Allow Concurrent Events
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.clock;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDate;
+
+public interface Clock {
+    ///  @return DateTimeZone
+    public DateTime getTime(DateTimeZone tz);
+
+    ///  @brief: get UTC now.
+    public DateTime getUTCNow();
+
+    ///  @brief get today UTC
+    public LocalDate getUTCToday();
+
+    /// @return DateTimeZone
+    public LocalDate getToday(DateTimeZone timeZone);
+
+
+}
