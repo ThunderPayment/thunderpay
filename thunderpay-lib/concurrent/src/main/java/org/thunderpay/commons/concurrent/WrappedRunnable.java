2e07e1690de527a82c780a5aa1603a6a34ccb3d0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-06 12:27:20 +0530

    thunderpay-lib: concurrent[WrappedRunnable]

--- thunderpay-lib/concurrent/src/main/java/org/thunderpay/commons/concurrent/WrappedRunnable.java
+++ thunderpay-lib/concurrent/src/main/java/org/thunderpay/commons/concurrent/WrappedRunnable.java
@@ -1,4 +1,39 @@
+/**
+ * @file WrappedRunnable.java
+ * @author Krisna Pranav
+ * @brief Wrapped Runnable
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
 package org.thunderpay.commons.concurrent;
 
-public class WrappedRunnable {
+import org.slf4j.Logger;
+
+class WrappedRunnable implements Runnable {
+    private final Logger log;
+    private final Runnable runnable;
+    private volatile Throwable exception;
+
+    private WrappedRunnable(final Logger log, final Runnable runnable) {
+        this.log = log;
+        this.runnable = runnable;
+    }
+
+    @Override
+    public void run() {
+        final Thread currentThread = Thread.curentThread();
+
+        try {
+            runnable.run();
+        } catch (final Throwable e) {
+            log.error(currentThread + "ended with an exception", e);
+            exception = e;
+        }
+
+        log.debug("{} finished executing", currentThread);
+    }
 }
