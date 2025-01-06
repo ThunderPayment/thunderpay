5cfb9a6243d4ab8045300adaa8d89531ddccbcb4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-06 11:34:08 +0530

    thunderpay-lib: concurrent[profiling(ProfilingData)]

--- thunderpay-lib/concurrent/src/main/java/org/thunderpay/commons/profiling/ProfilingData.java
+++ thunderpay-lib/concurrent/src/main/java/org/thunderpay/commons/profiling/ProfilingData.java
@@ -19,6 +19,7 @@ import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 
 public class ProfilingData {
+
     private final List<ProfilingDataItem> rawData;
     private final ProfilingFeature profileFeature;
 
@@ -28,14 +29,38 @@ public class ProfilingData {
     }
 
     public void merge(@Nullable final ProfilingData otherData) {
-        if (otherData == null || otherData.getRawData().isEmpty()) {
+        if (otherData == null ||
+                otherData.getRawData().isEmpty()) {
             return;
         }
-
         rawData.addAll(otherData.getRawData());
-
         Collections.sort(rawData, new Comparator<ProfilingDataItem>() {
+            @Override
+            public int compare(final ProfilingDataItem o1, final ProfilingDataItem o2) {
+                return o1.getTimestampNsec().compareTo(o2.getTimestampNsec());
+            }
         });
+    }
+
+    public void addStart(final ProfilingFeature.ProfilingFeatureType profileType, final String id) {
+        rawData.add(new ProfilingDataItem(profileType, id, LogLineType.START));
+    }
+
+    public void addEnd(final ProfilingFeature.ProfilingFeatureType profileType, final String id) {
+        rawData.add(new ProfilingDataItem(profileType, id, LogLineType.END));
+    }
+
+    public List<ProfilingDataItem> getRawData() {
+        if (rawData == null || rawData.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return rawData.stream()
+                .filter(input -> input != null && profileFeature.isDefined(input.getProfileType()))
+                .collect(Collectors.toList());
+    }
+
+    public ProfilingFeature getProfileFeature() {
+        return profileFeature;
     }
 
     public static class ProfilingDataItem {
@@ -50,5 +75,26 @@ public class ProfilingData {
             this.lineType = lineType;
             this.timestampNsec = System.nanoTime();
         }
+
+        public ProfilingFeature.ProfilingFeatureType getProfileType() {
+            return profileType;
+        }
+
+        public String getKey() {
+            return profileType + ":" + id;
+        }
+
+        public Long getTimestampNsec() {
+            return timestampNsec;
+        }
+
+        public LogLineType getLineType() {
+            return lineType;
+        }
+    }
+
+    public enum LogLineType {
+        START,
+        END
     }
 }
