3e7746dd8e92d0eff044b1cf3709f6d9e18e8f2c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-06 14:59:04 +0530

    thunderpay-api: billing[api(AuditLevel)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/util/api/AuditLevel.java
@@ -0,0 +1,18 @@
+/**
+ * @file Audit Level.java
+ * @author Krisna Pranav
+ * @brief Audit Level
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.util.api;
+
+public enum AuditLevel {
+    FULL,
+    MINIMAL,
+    NONE
+} // public enum AuditLevel
