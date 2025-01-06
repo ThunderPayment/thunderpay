10246963ec8edf712f26e07a9d43ca30f401a525
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-06 11:39:56 +0530

    thunderpay-lib: concurrent[profiling(Profiling)]

--- thunderpay-lib/concurrent/src/main/java/org/thunderpay/commons/profiling/Profiling.java
+++ thunderpay-lib/concurrent/src/main/java/org/thunderpay/commons/profiling/Profiling.java
@@ -11,14 +11,39 @@
 
 package org.thunderpay.commons.profiling;
 
-public class Profiling<ReturnType, ExeceptionType extends Throwable> {
+public class Profiling<ReturnType, ExceptionType extends Throwable> {
+
     private static final ThreadLocal<ProfilingData> perThreadProfilingData = new ThreadLocal<ProfilingData>();
 
     public interface WithProfilingCallback<ReturnType, ExceptionType extends Throwable> {
-        public ReturnType execute() throws ExeceptionType;
+        public ReturnType execute() throws ExceptionType;
     }
 
-    public ReturnType executeWithProfiling(final ProfilingFeature.ProfilingFeatureType profilingType, final String profilingId, final WithProfilingCallback<ReturnType, ExceptionType> callback) throws ExceptionType { {
+    public ReturnType executeWithProfiling(final ProfilingFeature.ProfilingFeatureType profilingType, final String profilingId, final WithProfilingCallback<ReturnType, ExceptionType> callback) throws ExceptionType {
+        final ProfilingData profilingData = Profiling.getPerThreadProfilingData();
+        if (profilingData == null) {
+            return callback.execute();
+        }
+        profilingData.addStart(profilingType, profilingId);
+        try {
+            return callback.execute();
+        } finally {
+            profilingData.addEnd(profilingType, profilingId);
+        }
+    }
+
+    public static ProfilingData getPerThreadProfilingData() {
+        return perThreadProfilingData.get();
+    }
+
+    public static void setPerThreadProfilingData() {
+        final ProfilingFeature profilingFeature = new ProfilingFeature();
+        perThreadProfilingData.set(new ProfilingData(profilingFeature));
+    }
+
+    public static void setPerThreadProfilingData(final String profileFeatures) {
+        final ProfilingFeature profilingFeature = new ProfilingFeature(profileFeatures);
+        perThreadProfilingData.set(new ProfilingData(profilingFeature));
     }
 
     public static void resetPerThreadProfilingData() {
