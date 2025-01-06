23147e4ea1c9ef05c2e8157d6f2016121c4e62e5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-06 10:53:57 +0530

    thunderpay-lib: clock[ClockUtil]

--- thunderpay-lib/clock/src/main/java/org/thunderpay/clock/ClockUtil.java
+++ thunderpay-lib/clock/src/main/java/org/thunderpay/clock/ClockUtil.java
@@ -18,11 +18,17 @@ import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 
 public class ClockUtil {
+
     public static DateTime toUTCDateTime(final LocalDate localDate, final LocalTime referenceTime, final DateTimeZone dateTimeZone) {
         DateTime targetDateTime;
         try {
-            targetDateTime = new DateTime(localDate.getYear(), localDate.getMonthOfYear(), localDate.getDayOfMonth(), referenceTime.getHourOfDay(), referenceTime.getMinuteOfHour(), referenceTime.getSecondOfMinute(), dateTimeZone);
-
+            targetDateTime = new DateTime(localDate.getYear(),
+                    localDate.getMonthOfYear(),
+                    localDate.getDayOfMonth(),
+                    referenceTime.getHourOfDay(),
+                    referenceTime.getMinuteOfHour(),
+                    referenceTime.getSecondOfMinute(),
+                    dateTimeZone);
         } catch (final IllegalInstantException e) {
             targetDateTime = localDate.toDateTimeAtStartOfDay(dateTimeZone);
         }
@@ -34,7 +40,11 @@ public class ClockUtil {
         return new LocalDate(dateTime, dateTimeZone);
     }
 
-    public static DateTimeZone toUTCDateTime(final DateTime dateTime) {
+    public static DateTime toUTCDateTime(final DateTime dateTime) {
         return toDateTime(dateTime, DateTimeZone.UTC);
+    }
+
+    public static DateTime toDateTime(final DateTime dateTime, final DateTimeZone accountTimeZone) {
+        return dateTime.toDateTime(accountTimeZone);
     }
 }
