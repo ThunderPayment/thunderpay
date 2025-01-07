dd15df6fbe17612175ee71a731180c3868bc15d4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-07 19:27:54 +0530

    thunderpay-api: billing[ThunderExceptionBase]

--- thunderpay-api/src/main/java/org/thunderpay/billing/ThunderExceptionBase.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/ThunderExceptionBase.java
@@ -12,5 +12,22 @@
 package org.thunderpay.billing;
 
 public class ThunderExceptionBase extends Exception {
-    
+    private static final long serialVersionUID = 165720101383L;
+    private final Throwable cause;
+    private final int code;
+    private final String formattedMsg;
+
+    public ThunderExceptionBase(final Throwable cause, final int code, final String msg) {
+        this.cause = cause;
+        this.code = code;
+        this.formattedMsg = msg;
+    }
+
+    public ThunderExceptionBase(final ThunderExceptionBase cause) {
+        this(cause, cause.getCode(), cause.getMessage());
+    }
+
+    public int getCode() {
+        return code;
+    }
 }
