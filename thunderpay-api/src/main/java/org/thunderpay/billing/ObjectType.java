8c03e936c1eb81f7b5503a898b9323c985d3371d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-07 19:23:08 +0530

    thunderpay-api: billing[ObjectType]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/ObjectType.java
@@ -0,0 +1,34 @@
+/**
+ * @file ObjectType.java
+ * @author Krisna Pranav
+ * @brief Object Type
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing;
+
+public enum ObjectType {
+    ACCOUNT,
+    ACCOUNT_EMAIL,
+    BLOCKING_STATES,
+    BUNDLE,
+    CUSTOM_FIELD,
+    INVOICE,
+    PAYMENT,
+    TRANSACTION,
+    INVOICE_ITEM,
+    INVOICE_PAYMENT,
+    SUBSCRIPTION,
+    SUBSCRIPTION_EVENT,
+    SERVICE_BROADCAST,
+    PAYMENT_ATTEMPT,
+    PAYMENT_METHOD,
+    TAG,
+    TAG_DEFINITION,
+    TENANT,
+    TENANT_KVS;
+} // public enum ObjectType
