7a5de56191996088d937a1391f7e18fec42959b7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-07 19:24:06 +0530

    thunderpay-api: billing[OrderingType]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/OrderingType.java
@@ -0,0 +1,17 @@
+/**
+ * @file OrderingType.java
+ * @author Krisna Pranav
+ * @brief Ordering Type
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing;
+
+public enum OrderingType {
+    ASCENDING,
+    DESCENDING
+}
