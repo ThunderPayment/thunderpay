ef8052c47a3e666b018c2785a3e541ef73e81cc9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-07 19:25:29 +0530

    thunderpay-api: billing[ThunderAPI]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/ThunderApi.java
@@ -0,0 +1,16 @@
+/**
+ * @file ThunderApi
+ * @author Krisna Pranav
+ * @brief Thunder API
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+
+package org.thunderpay.billing;
+
+public interface ThunderApi {
+}
