3a1dbb242c61af1aa1f6469359b04a2910bf9700
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-07 18:51:59 +0530

    thunderpay-lib: skeleton

---
+++ thunderpay-lib/skeleton/src/main/java/org/thunderpay/skeleton/metrics/ResourceTimer.java
@@ -0,0 +1,40 @@
+/**
+ * @file ResourceTimer.java
+ * @author Krisna Pranav
+ * @brief Resource Timer
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.skeleton.metrics;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Nullable;
+import org.thunderpay.metrics.api.MetricRegistry;
+import org.thunderpay.metrics.api.Timer;
+import org.thunderpay.commons.utils.Joiner;
+
+public class ResourceTimer {
+    private static final Joiner METRIC_NAME_JOINER = Joiner.on('.');
+    private final String resourcePath;
+    private final String name;
+    private final String httpMethod;
+    private final Map<String, Object> tags;
+    private final MetricRegistry registry;
+
+    public ResourceTimer(final String resourcePath, final String name, final String httpMethod, @Nullable final Map<String, Object> tags, final MetricRegistry registry) {
+        this.resourcePath = resourcePath;
+        this.name = name;
+        this.httpMethod = httpMethod;
+        this.tags = tags == null ? null : new HashMap<>(tags);
+        this.registry = registry;
+    }
+    
+}
