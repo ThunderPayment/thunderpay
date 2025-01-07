76df89880296fa705e0b7a4206a48909e76fd8b5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-07 19:26:17 +0530

    thunderpay-api: billing[ThunderExceptionBase]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/ThunderExceptionBase.java
@@ -0,0 +1,16 @@
+/**
+ * @file ThunderExceptionBase.java
+ * @author Krisna Pranav
+ * @brief Thunder Exception Base
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing;
+
+public class ThunderExceptionBase extends Exception {
+    
+}
