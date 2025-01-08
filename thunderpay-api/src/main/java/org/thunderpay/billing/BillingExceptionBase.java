5956356280d54d5e0852dced522f8a1366b56f54
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-08 18:24:55 +0530

    thunderpay-api: billing[BillingExceptionBase]

--- thunderpay-api/src/main/java/org/thunderpay/billing/BillingExceptionBase.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/BillingExceptionBase.java
@@ -1,17 +1,40 @@
+/**
+ * @file BillingExceptionBase.java
+ * @author Krisna Pranav
+ * @brief Billing Exception Base
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
 package org.thunderpay.billing;
 
-public class BillingExceptionBase extends Exception{
+public class BillingExceptionBase extends Exception {
+
     private static final long serialVersionUID = 165720101383L;
 
     private final Throwable cause;
     private final int code;
     private final String formattedMsg;
 
-
     public BillingExceptionBase(final Throwable cause, final int code, final String msg) {
         this.cause = cause;
         this.code = code;
         this.formattedMsg = msg;
+    }
+
+    public BillingExceptionBase(final BillingExceptionBase cause) {
+        this(cause, cause.getCode(), cause.getMessage());
+    }
+
+    public BillingExceptionBase(final Throwable cause, final ErrorCode code, final Object... args) {
+        this(cause, code.getCode(), String.format(code.getFormat(), args));
+    }
+
+    public BillingExceptionBase(final ErrorCode code, final Object... args) {
+        this(null, code, args);
     }
 
     @Override
@@ -36,9 +59,5 @@ public class BillingExceptionBase extends Exception{
         sb.append(", formattedMsg='").append(formattedMsg).append('\'');
         sb.append('}');
         return sb.toString();
-    }
-
-    public BillingExceptionBase(final BillingExceptionBase cause) {
-        this(cause, cause.getCode(), cause.getMessage());
     }
 }
