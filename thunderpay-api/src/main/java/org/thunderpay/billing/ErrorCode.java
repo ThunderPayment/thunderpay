3d9b46b297899fdc2cad48020919f12b7896a38b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-08 18:24:46 +0530

    thunderpay-api: billing[ErrorCode]

--- thunderpay-api/src/main/java/org/thunderpay/billing/ErrorCode.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/ErrorCode.java
@@ -1,3 +1,14 @@
+/**
+ * @file ErrorCode.java
+ * @author Krisna Pranav
+ * @brief Error Code
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
 package org.thunderpay.billing;
 
 public class ErrorCode {
