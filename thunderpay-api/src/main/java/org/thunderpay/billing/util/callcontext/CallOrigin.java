078f089665b4792c4a11d09d662a35272b2d6e77
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-09 18:19:57 +0530

    thunderpay-api: billing[callcontext(CallOrigin)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/util/callcontext/CallOrigin.java
@@ -0,0 +1,18 @@
+/**
+ * @file CallOrigin.java
+ * @author Krisna Pranav
+ * @brief Call Origin
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.util.callcontext;
+
+public enum CallOrigin {
+    INTERNAL,
+    EXTERNAL,
+    TEST;
+}
