f34951d04600bb79e23a7453bd71467dfce514fd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-09 18:16:37 +0530

    thunderpay-api: billing[callcontext(UserType)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/util/callcontext/UserType.java
@@ -0,0 +1,20 @@
+/**
+ * @file UserType.java
+ * @author Krisna Pranav
+ * @brief User Type
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.util.callcontext;
+
+public enum UserType {
+    SYSTEM,
+    ADMIN,
+    CUSTOMER,
+    MIGRATION,
+    TEST;
+}
