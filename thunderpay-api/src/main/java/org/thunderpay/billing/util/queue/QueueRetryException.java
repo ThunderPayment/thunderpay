78c201396039ac25d7fefcd983c7cdd203217d76
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-10 18:38:23 +0530

    thunderpay-api: billing[queue(QueueRetryException)]

--- thunderpay-api/src/main/java/org/thunderpay/billing/util/queue/QueueRetryException.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/util/queue/QueueRetryException.java
@@ -15,8 +15,8 @@ import java.util.List;
 import org.joda.time.Period;
 
 public class QueueRetryException extends RuntimeException {
-    public static final List<Period> DEFAULT_RETRY_SCHEDULE = List.of(Period.minutes(5));
 
+    public static final List<Period> DEFAULT_RETRY_SCHEDULE = List.of(Period.minutes(5), Period.minutes(15), Period.hours(1), Period.hours(6), Period.hours(24));
     private final List<Period> retrySchedule;
 
     public QueueRetryException() {
@@ -25,5 +25,23 @@ public class QueueRetryException extends RuntimeException {
 
     public QueueRetryException(final Exception e) {
         this(e, null);
+    }
+
+    public QueueRetryException(final List<Period> retrySchedule) {
+        this(null, retrySchedule);
+    }
+
+    public QueueRetryException(final Exception e, final List<Period> retrySchedule) {
+        super(e);
+        this.retrySchedule = retrySchedule != null ? retrySchedule : DEFAULT_RETRY_SCHEDULE;
+    }
+
+    public List<Period> getRetrySchedule() {
+        return List.copyOf(retrySchedule);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (retrySchedule: %s)", super.toString(), retrySchedule);
     }
 }
