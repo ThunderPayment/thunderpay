b65e27812aa701f6594ba4de9e7fc702bd792b21
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-11 16:07:39 +0530

    thunderpay-api: billing[osgi(PluginStateChange)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/osgi/api/PluginStateChange.java
@@ -0,0 +1,18 @@
+/**
+ * @file PluginStateChange.java
+ * @author Krisna Pranav
+ * @brief Plugin State Change
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.osgi.api;
+
+public enum PluginStateChange {
+    NEW_VERSION,
+
+    DISABLED
+}
