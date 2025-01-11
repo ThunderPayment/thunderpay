0e9d0e4b4ec1fe67f501fe6fd4b96b0e6a330f0e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-11 11:36:50 +0530

    thunderpay-api: billing[customfield(CustomField)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/util/customfield/CustomField.java
@@ -0,0 +1,22 @@
+/**
+ * @file CustomField.java
+ * @author Krisna Pranav
+ * @brief Custom Field
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.util.customfield;
+
+import java.util.UUID;
+import org.thunderpay.billing.ObjectType;
+import org.thunderpay.billing.util.entity.Entity;
+
+public interface CustomField extends Entity {
+    public UUID getObjectId();
+
+    public ObjectType getObjectType();
+}
