505cffca62b719d79a283e368268a77fc9655e45
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-11 11:32:10 +0530

    thunderpay-api: billing[TagDefinition]

--- thunderpay-api/src/main/java/org/thunderpay/billing/util/tag/TagDefinition.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/util/tag/TagDefinition.java
@@ -18,5 +18,12 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 public interface TagDefinition extends Entity {
+
     String getName();
+
+    String getDescription();
+
+    Boolean isControlTag();
+
+    List<ObjectType> getApplicableObjectTypes();
 }
