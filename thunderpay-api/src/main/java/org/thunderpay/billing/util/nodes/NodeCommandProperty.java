3f3849ac54777bf49b499398d51f34144e67c47e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-11 12:24:21 +0530

    thunderpay-api: billing[nodes(NodeCommandProperty)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/util/nodes/NodeCommandProperty.java
@@ -0,0 +1,32 @@
+/**
+ * @file NodeCommandProperty.java
+ * @author Krisna Pranav
+ * @brief Node Command Property
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.util.nodes;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class NodeCommandProperty {
+    private String key;
+    private Object value;
+
+    public NodeCommandProperty() { }
+
+    @JsonCreator
+    public NodeCommandProperty(@JsonProperty("key") final String key, @JsonProperty("value") final Object value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+}
