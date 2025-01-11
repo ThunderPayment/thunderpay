322abb1577001335902fa29d7355e437591c0c3d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-11 15:23:10 +0530

    thunderpay-api: billing[nodes(DefaultNodeCommandMetadata)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/util/nodes/DefaultNodeCommandMetadata.java
@@ -0,0 +1,27 @@
+/**
+ * @file DefaultNodeCommandMetadata.java
+ * @author Krisna Pranav
+ * @brief Default Node Command Metadata
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.util.nodes;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class DefaultNodeCommandMetadata implements NodeCommandMetadata {
+    private List<NodeCommandProperty> properties;
+
+    public DefaultNodeCommandMetadata() {}
+
+    @JsonCreator
+    public DefaultNodeCommandMetadata(@JsonProperty("properties") final List<NodeCommandProperty> properties) {
+        this.properties = List.copyOf(properties);
+    }
+}
