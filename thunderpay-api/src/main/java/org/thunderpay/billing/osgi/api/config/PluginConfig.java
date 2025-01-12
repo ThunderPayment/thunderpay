160692c3ee61e288066020ed2411f3101eb4f555
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-12 14:42:46 +0530

    thunderpay-api: billing[config(PluginConfig)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/osgi/api/config/PluginConfig.java
@@ -0,0 +1,22 @@
+/**
+ * @file PluginConfig.java
+ * @author Krisna Pranav
+ * @brief Plugin Config
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.osgi.api.config;
+
+import java.io.File;
+
+public interface PluginConfig extends Comparable<PluginConfig> {
+    public String getPluginKey();
+
+    public String getPluginName();
+
+    public String getVersion();
+}
