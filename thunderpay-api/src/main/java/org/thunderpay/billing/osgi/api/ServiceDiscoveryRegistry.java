4ee73a1399d5c098f14b1c5662c6146e37901499
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-12 16:30:23 +0530

    thunderpay-api: billing[osgi(ServiceDiscoveryRegistry)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/osgi/api/ServiceDiscoveryRegistry.java
@@ -0,0 +1,16 @@
+/**
+ * @file ServiceDiscoveryRegistry.java
+ * @author Krisna Pranav
+ * @brief Service Discovery Registry
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.osgi.api;
+
+public interface ServiceDiscoveryRegistry {
+    void register();
+}
