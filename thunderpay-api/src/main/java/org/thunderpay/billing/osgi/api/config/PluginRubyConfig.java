293005b8dbcbf457f6bbc29f164afe7dabaad99d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-12 15:42:27 +0530

    thunderpay-api: billing[config(PluginRubyConfig)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/osgi/api/config/PluginRubyConfig.java
@@ -0,0 +1,16 @@
+/**
+ * @file PluginRubyConfig.java
+ * @author Krisna Pranav
+ * @brief Plugin Ruby Config
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.osgi.api.config;
+
+public interface PluginRubyConfig extends PluginConfig {
+    public String getRubyMainClass();
+}
