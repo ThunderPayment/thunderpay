41ebbd36932aae8f5fadc26d916d385840766570
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-12 14:41:29 +0530

    thunderpay-api: billing[osgi(PluginLanguage)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/osgi/api/config/PluginLanguage.java
@@ -0,0 +1,17 @@
+/**
+ * @file PluginLanguage.java
+ * @author Krisna Pranav
+ * @brief Plugin Language
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.osgi.api.config;
+
+public enum PluginLanguage {
+    JAVA,
+    RUBY
+}
