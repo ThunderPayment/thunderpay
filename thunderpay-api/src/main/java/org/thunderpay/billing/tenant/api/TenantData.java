0d36950bea32b29406916656b13caf59fdd36167
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-14 11:58:36 +0530

    thunderpay-api: billing[tenant(TenantData)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/tenant/api/TenantData.java
@@ -0,0 +1,18 @@
+/**
+ * @file TenantData.java
+ * @author Krisna Pranav
+ * @brief Tenant Data
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.tenant.api;
+
+public interface TenantData {
+    public String getExternalKey();
+
+    public String getApiKey();
+}
