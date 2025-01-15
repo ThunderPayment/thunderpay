a6ed6d652732bdb67bf1d438e22f7fc7156e1c81
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-15 16:18:39 +0530

    thunderpay-api: billing[security(Logical)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/security/Logical.java
@@ -0,0 +1,17 @@
+/**
+ * @file Logical.java
+ * @author Krisna Pranav
+ * @brief Logical
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.security;
+
+public enum Logical {
+    AND,
+    OR
+}
