ad2cf982bcf3d33611f8221b31107f9699a880de
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-16 16:35:00 +0530

    thunderpay-api: billing[security(RequiresPermission)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/security/RequiresPermissions.java
@@ -0,0 +1,23 @@
+/**
+ * @file RequiresPermission.java
+ * @author Krisna Pranav
+ * @brief Requires Permission
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.security;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequiresPermissions {
+    Permission[] value();
+}
