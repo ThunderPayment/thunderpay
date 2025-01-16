dd9f3e07b22118811f34fb53fc2c328388963a89
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-16 16:36:36 +0530

    thunderpay-api: billing[security(SecurityAPIException)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/security/SecurityApiException.java
@@ -0,0 +1,21 @@
+/**
+ * @file SecurityApiException.java
+ * @author Krisna Pranav
+ * @brief Security API Exception
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.security;
+
+import org.thunderpay.billing.BillingExceptionBase;
+import org.thunderpay.billing.ErrorCode;
+
+public class SecurityApiException extends BillingExceptionBase {
+    public SecurityApiException(final Throwable cause, final int code, final String msg) {
+        super(cause, code, msg);
+    }
+}
