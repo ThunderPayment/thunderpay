caa73a54b84aace47ba93cf5fc6ed2eb370945fc
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-17 12:03:44 +0530

    thunderpay-api: billing[security(SecurityApi)]

--- thunderpay-api/src/main/java/org/thunderpay/billing/security/api/SecurityApi.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/security/api/SecurityApi.java
@@ -27,11 +27,38 @@ import org.thunderpay.billing.util.callcontext.TenantContext;
 import static org.thunderpay.billing.security.Permission.TENANT_CAN_CREATE;
 import static org.thunderpay.billing.security.Permission.USER_CAN_CREATE;
 
-public class SecurityApi extends ThunderApi {
+public interface SecurityApi extends ThunderApi {
     public void login(Object principal, Object credentials);
 
     public void logout();
 
+    public boolean isSubjectAuthenticated();
+
+    Set<String> getCurrentUserPermissions(TenantContext context);
+
+    void checkCurrentUserPermissions(List<Permission> permissions, Logical logical, TenantContext context) throws SecurityApiException;
+
+    @RequiresPermissions(USER_CAN_CREATE)
+    public void addUserRoles(String username, String clearPassword, List<String> roles, CallContext context) throws SecurityApiException;
+
+    @RequiresPermissions(USER_CAN_CREATE)
+    public void updateUserPassword(String username, String clearPassword, CallContext context) throws SecurityApiException;
+
+    @RequiresPermissions(USER_CAN_CREATE)
+    public void updateUserRoles(String username, List<String> roles, CallContext context) throws SecurityApiException;
+
+    @RequiresPermissions(USER_CAN_CREATE)
+    public void invalidateUser(String username, CallContext context) throws SecurityApiException;
+
+    public List<String> getUserRoles(String username, final TenantContext tenantContext) throws SecurityApiException;
+
+    @RequiresPermissions(USER_CAN_CREATE)
+    public void addRoleDefinition(String role, List<String> permissions, CallContext context) throws SecurityApiException;
+
+
     @RequiresPermissions(USER_CAN_CREATE)
     public void updateRoleDefinition(String role, List<String> permissions, CallContext context) throws SecurityApiException;
+
+    public List<String> getRoleDefinition(final String role, final TenantContext tenantContext);
+
 }
