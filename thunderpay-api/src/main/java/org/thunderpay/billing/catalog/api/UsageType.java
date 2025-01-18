66f60bf31d719401c62c435ddb2a1ebfece7d7e9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-18 12:17:56 +0530

    thunderpay-api: billing[catalog(UsageType)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/UsageType.java
@@ -0,0 +1,17 @@
+/**
+ * @file VersionedCatalog.java
+ * @author Krisna Pranav
+ * @brief Versioned Catalog
+ * @version 1.0
+ * @date 2025-01-18
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public enum UsageType {
+    CAPACITY,
+    CONSUMABLE
+}
