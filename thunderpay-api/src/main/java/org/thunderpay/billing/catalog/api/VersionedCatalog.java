b033c5233680d53addef6de648dc9c529614b39b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-18 12:15:54 +0530

    thunderpay-api: billing[catalog(VersionedCatalog)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/VersionedCatalog.java
@@ -0,0 +1,21 @@
+/**
+ * @file VersionedCatalog.java
+ * @author Krisna Pranav
+ * @brief Versioned Catalog
+ * @version 1.0
+ * @date 2025-01-18
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+import java.util.Date;
+import java.util.List;
+
+public interface VersionedCatalog {
+    public String getCatalogName();
+
+    public StaticCatalog getVersion(Date targetDate);
+}
