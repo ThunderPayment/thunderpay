9d9db3be4792da1ae970a91dd0b3e1bc9f37dd5d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-18 12:19:56 +0530

    thunderpay-api: billing[catalog(BillingAlignment)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/BillingAlignment.java
@@ -0,0 +1,17 @@
+/**
+ * @file BillingAlignment.java
+ * @author Krisna Pranav
+ * @brief Billing Alignment
+ * @version 1.0
+ * @date 2025-01-18
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public enum BillingAlignment {
+    ACCOUNT,
+    BUNDLE,
+}
