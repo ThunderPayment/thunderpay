fecde8793934cb19f71c99aa71d139f850f8cc2a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-18 12:12:51 +0530

    thunderpay-api: billing[entitlement(SubscriptionEventType)]

--- thunderpay-api/src/main/java/org/thunderpay/billing/entitlement/api/SubscriptionEventType.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/entitlement/api/SubscriptionEventType.java
@@ -14,6 +14,18 @@ package org.thunderpay.billing.entitlement.api;
 import org.thunderpay.billing.ObjectType;
 
 public enum SubscriptionEventType {
+    START_ENTITLEMENT(ObjectType.BLOCKING_STATES),
+    START_BILLING(ObjectType.SUBSCRIPTION_EVENT),
+    PAUSE_ENTITLEMENT(ObjectType.BLOCKING_STATES),
+    PAUSE_BILLING(ObjectType.BLOCKING_STATES),
+    RESUME_ENTITLEMENT(ObjectType.BLOCKING_STATES),
+    RESUME_BILLING(ObjectType.BLOCKING_STATES),
+    PHASE(ObjectType.SUBSCRIPTION_EVENT),
+    CHANGE(ObjectType.SUBSCRIPTION_EVENT),
+    STOP_ENTITLEMENT(ObjectType.BLOCKING_STATES),
+    STOP_BILLING(ObjectType.SUBSCRIPTION_EVENT),
+    SERVICE_STATE_CHANGE(ObjectType.BLOCKING_STATES);
+
     private ObjectType objectType;
 
     SubscriptionEventType(ObjectType type) {
