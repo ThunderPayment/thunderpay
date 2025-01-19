96eb5fd5ef1323c4b85f58f20310afca67cc6adb
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-19 12:44:10 +0530

    thunderpay-api: billing[catalog(BillingMode)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/BillingMode.java
@@ -0,0 +1,17 @@
+/**
+ * @file BillingMode.java
+ * @author Krisna Pranav
+ * @brief Billing Mode
+ * @version 1.0
+ * @date 2025-01-19
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public enum BillingMode {
+    IN_ADVANCE,
+    IN_ARREAR
+}
