cf6c383fd548da6f0d45e89088f819c89981f337
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-19 12:12:02 +0530

    thunderpay-api: billing[catalog(BillingActionPolicy)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/BillingActionPolicy.java
@@ -0,0 +1,19 @@
+/**
+ * @file BillingActionPolicy
+ * @author Krisna Pranav
+ * @brief Billing Action Policy
+ * @version 1.0
+ * @date 2025-01-19
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public enum BillingActionPolicy {
+    START_OF_TERM,
+    END_OF_TERM,
+    IMMEDIATE,
+    ILLEGAL
+}
