93ac1e65f8d3b7d5acedf1869d5858707b79a7fa
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-20 18:47:56 +0530

    thunderpay-api: billing[catalog(Block)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/Block.java
@@ -0,0 +1,15 @@
+/**
+ * @file Block.java
+ * @author Krisna Pranav
+ * @brief Block
+ * @version 1.0
+ * @date 2025-01-20
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public class Block {
+}
