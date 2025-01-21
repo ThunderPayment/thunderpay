027e7623a22919c422c2f3798022a98702d41f92
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-21 16:57:33 +0530

    thunderpay-api: billing[catalog(Unit)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/Unit.java
@@ -0,0 +1,14 @@
+/**
+ * @file Unit.java
+ * @author Krisna Pranav
+ * @brief Units
+ * @version 1.0
+ * @date 2025-01-21
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public interface Unit extends CatalogEntity { }
