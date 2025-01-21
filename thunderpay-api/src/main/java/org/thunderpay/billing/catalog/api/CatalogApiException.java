de49c8dc3961a0f9c778e6fb850cdbe1f7fa12ca
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-21 17:04:14 +0530

    thunderpay-api: billing[catalog(CatalogApiException)]

--- thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/CatalogApiException.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/CatalogApiException.java
@@ -20,5 +20,12 @@ public class CatalogApiException extends BillingExceptionBase {
     public CatalogApiException(final BillingExceptionBase cause) {
         super(cause);
     }
-    
+
+    public CatalogApiException(final Throwable cause, final ErrorCode code, final Object... args) {
+        super(cause, code, args);
+    }
+
+    public CatalogApiException(final ErrorCode code, final Object... args) {
+        super(code, args);
+    }
 }
