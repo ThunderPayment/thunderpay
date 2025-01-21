d8ef5e9210c16a4aafe10a3931807768de7fe565
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-21 17:03:34 +0530

    thunderpay-api: billing[catalog(CatalogApiException)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/CatalogApiException.java
@@ -0,0 +1,24 @@
+/**
+ * @file CatalogApiException.java
+ * @author Krisna Pranav
+ * @brief Catalog Api Exception
+ * @version 1.0
+ * @date 2025-01-21
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+import org.thunderpay.billing.BillingExceptionBase;
+import org.thunderpay.billing.ErrorCode;
+
+public class CatalogApiException extends BillingExceptionBase {
+    private static final long serialVersionUID = 1L;
+
+    public CatalogApiException(final BillingExceptionBase cause) {
+        super(cause);
+    }
+    
+}
