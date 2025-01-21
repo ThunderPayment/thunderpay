98654bb39481c0155a20d9a9d20664e3d1c38535
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-21 16:58:24 +0530

    thunderpay-api: billing[catalog(CatalogEntity)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/CatalogEntity.java
@@ -0,0 +1,18 @@
+/**
+ * @file CatalogEntity.java
+ * @author Krisna Pranav
+ * @brief Catalog Entity
+ * @version 1.0
+ * @date 2025-01-21
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public interface CatalogEntity {
+    public String getName();
+
+    public String getPrettyName();
+}
