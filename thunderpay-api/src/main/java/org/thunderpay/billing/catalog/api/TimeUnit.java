724dba3122f4c9220178e7bc6bf03f6a437bdafd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-23 18:52:55 +0530

    thunderpay-api: billing[catalog(TimeUnit)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/TimeUnit.java
@@ -0,0 +1,23 @@
+/**
+ * @file TimeUnit.java
+ * @author Krisna Pranav
+ * @brief Time Unit
+ * @version 1.0
+ * @date 2025-01-23
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+import javax.xml.bind.annotation.XmlEnum;
+
+@XmlEnum
+public enum TimeUnit {
+    DAYS,
+    WEEKS,
+    MONTHS,
+    YEARS,
+    UNLIMITED
+}
