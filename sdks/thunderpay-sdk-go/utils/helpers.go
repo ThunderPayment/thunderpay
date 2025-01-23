50460e56aae4ec4e0ee3a5262f4737f57f4fe7b7
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-23 20:24:19 +0530

    thunderpay-sdk-go: utils[helpers]

--- sdks/thunderpay-sdk-go/utils/helpers.go
+++ sdks/thunderpay-sdk-go/utils/helpers.go
@@ -11,8 +11,39 @@
 
 package utils
 
+import (
+	"strconv"
+	"strings"
+)
+
 type FormFile struct {
 	FileBytes    []byte
 	FileName     string
 	FormFileName string
+}
+
+func Atoi(in string) (int, error) {
+	return strconv.Atoi(in)
+}
+
+func SelectHeaderAccept(accepts []string) string {
+	if len(accepts) == 0 {
+		return ""
+	}
+
+	if Contains(accepts, "application/json") {
+		return "application/json"
+	}
+
+	return strings.Join(accepts, ",")
+}
+
+func Contains(haystack []string, needle string) bool {
+	for _, a := range haystack {
+		if strings.EqualFold(a, needle) {
+			return true
+		}
+	}
+
+	return false
 }
