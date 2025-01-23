5b49ca9af27101c46f75e0b0e95f7f7a2b6c1c96
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-23 18:49:40 +0530

    thunderpay-api: billing[catalog(TierBlockPolicy)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/TierBlockPolicy.java
@@ -0,0 +1,17 @@
+/**
+ * @file TierBlockPolicy.java
+ * @author Krisna Pranav
+ * @brief Tier Block Policy
+ * @version 1.0
+ * @date 2025-01-21
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public enum TierBlockPolicy {
+    ALL_TIERS,
+    TOP_TIER
+}
