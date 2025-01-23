eef4e0574da96fab15fb1a2998c7e5b1ff6d90ea
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-23 20:19:33 +0530

    sdks: thunderpay-sdk-go

---
+++ sdks/thunderpay-sdk-go/utils/helpers.go
@@ -0,0 +1,18 @@
+/**
+ * @file helpers.go
+ * @author Krisna Pranav
+ * @brief Helpers
+ * @version 1.0
+ * @date 2024-01-23
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package utils
+
+type FormFile struct {
+	FileBytes    []byte
+	FileName     string
+	FormFileName string
+}
