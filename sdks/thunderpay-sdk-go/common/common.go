ab617031ccca933800e13eb4c5e72d7b7bef0034
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-25 22:36:40 +0530

    thunderpay-sdk-go: common

---
+++ sdks/thunderpay-sdk-go/common/common.go
@@ -0,0 +1,44 @@
+/**
+ * @file common.go
+ * @author Krisna Pranav
+ * @brief common
+ * @version 1.0
+ * @date 2024-01-23
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package common
+
+import (
+	"context"
+	"net/http"
+	"net/url"
+)
+
+type IConfiguration interface {
+	AddDefaultHeader(key string, value string)
+	ServerURL(index int, variables map[string]string) (string, error)
+	ServerURLWithContext(ctx context.Context, endpoint string) (string, error)
+}
+
+type IClient interface {
+	GetConfig() IConfiguration
+	PrepareRequest(
+		ctx context.Context,
+		path string, method string,
+		postBody interface{},
+		headerParams map[string]string,
+		queryParams url.Values,
+		formParams url.Values,
+		formFiles []FormFile) (localVarRequest *http.Request, err error)
+	CallAPI(request *http.Request) (*http.Response, error)
+	Decode(v interface{}, b []byte, contentType string) (err error)
+}
+
+type FormFile struct {
+	FileBytes    []byte
+	FileName     string
+	FormFileName string
+}
