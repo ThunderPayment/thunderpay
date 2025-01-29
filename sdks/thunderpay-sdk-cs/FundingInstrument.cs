a16d0b1f2b92bbf52fefd1b68f9f52d4f0924c21
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-29 15:11:04 +0530

    sdks: thunderpay-sdk-cs[FundingInstrument]

---
+++ sdks/thunderpay-sdk-cs/FundingInstrument.cs
@@ -0,0 +1,25 @@
+/**
+ * @file FundingInstrument.cs
+ * @author Krisna Pranav
+ * @brief Funding Instrument
+ * @version 1.0
+ * @date 2024-01-19
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+using System;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ThunderPay
+{
+    public abstract class FundingInstrument : Resource
+    {
+
+    } // public abstract class FundingInstrument : Resource
+} // namespace ThunderPay
