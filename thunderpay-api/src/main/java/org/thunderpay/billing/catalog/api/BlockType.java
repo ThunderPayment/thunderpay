d5c7ce5b3a5781a8958d0bd261b1f1a20af380cd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-29 12:10:37 +0530

    thunderpay-api: billing[catalog(BlockType)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/BlockType.java
@@ -0,0 +1,18 @@
+/**
+ * @file BlockType.java
+ * @author Krisna Pranav
+ * @brief Block Types
+ * @version 1.0
+ * @date 2025-01-21
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public enum BlockType {
+    VANILLA,
+    TOP_UP,
+    TIERED
+}
