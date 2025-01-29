3c87b2b9c27a55ac5c47b3c5434d8946b925a5a1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-29 12:51:41 +0530

    sdks: thunderpay-sdk-cs[Exceptions(FundingInstrumentNotDebitable)]

---
+++ sdks/thunderpay-sdk-cs/Exceptions/FundingInstrumentNotDebitable.cs
@@ -0,0 +1,16 @@
+/**
+ * @file FundingInstrumentNotDebitable.cs
+ * @author Krisna Pranav
+ * @brief Funding Instrument Not Debitable
+ * @version 1.0
+ * @date 2024-01-29
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
