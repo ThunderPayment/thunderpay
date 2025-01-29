632d18ec39f42e5a9088b4d814d2db19a62cbcf6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-29 12:50:38 +0530

    sdks: thunderpay-sdk-cs[Exceptions(FundingInstrumentNotCreditable)]

---
+++ sdks/thunderpay-sdk-cs/Exceptions/FundingInstrumentNotCreditable.cs
@@ -0,0 +1,21 @@
+/**
+ * @file FundingInstrumentNotCreditable.cs
+ * @author Krisna Pranav
+ * @brief Funding Instrument Not Creditable
+ * @version 1.0
+ * @date 2024-01-29
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ThunderPay.Exception
+{
+    public class FundingInstrumentNotCreditable : Exception { }
+}
