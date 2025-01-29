ce5d446210bf422300a9c1243b682bec9d07e5f1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-29 17:34:01 +0530

    sdks: thunderpay-sdk-cs[BankAccount]

--- sdks/thunderpay-sdk-cs/BankAccount.cs
+++ sdks/thunderpay-sdk-cs/BankAccount.cs
@@ -28,5 +28,35 @@ namespace ThunderPay
                 return "/bank_accounts";
             }
         } // public static string resource_href
+
+        [ResourceField]
+        public string account_type
+        {
+            get; set;
+        } // public string account_type 
+
+        [ResourceField]
+        public string account_number
+        {
+            get; set;
+        } // public string account_number 
+
+        [ResourceField]
+        public Dictionary<string, string> address
+        {
+            get; set;
+        } // public Dictionary<string, string> address 
+
+        [ResourceField]
+        public string name
+        {
+            get; set;
+        } // public string name 
+
+        [ResourceField]
+        public string routing_number
+        {
+            get; set;
+        } // public string routing_number 
     } // public class BankAccount : FundingInstrument
 } // namespace ThunderPay
