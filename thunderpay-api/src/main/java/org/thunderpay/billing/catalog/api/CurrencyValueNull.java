e06bdc7ec3a4b8a5bf6b18175910ac37f710fd80
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-29 12:15:36 +0530

    thunderpay-api: billing[catalog(CurrencyValueNull)]

--- thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/CurrencyValueNull.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/CurrencyValueNull.java
@@ -14,9 +14,14 @@ package org.thunderpay.billing.catalog.api;
 import org.thunderpay.billing.ErrorCode;
 
 public class CurrencyValueNull extends CatalogApiException {
+
     private static final long serialVersionUID = 1L;
 
     public CurrencyValueNull(final Throwable cause, final Object... args) {
         super(cause, ErrorCode.CAT_PRICE_VALUE_NULL_FOR_CURRENCY, args);
+    }
+
+    public CurrencyValueNull(final Object... args) {
+        super(ErrorCode.CAT_PRICE_VALUE_NULL_FOR_CURRENCY, args);
     }
 }
