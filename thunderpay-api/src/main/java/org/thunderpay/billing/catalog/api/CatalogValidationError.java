ec13d2fe428766ec0933c626549dbfecc2cc7890
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-29 12:13:12 +0530

    thunderpay-api: billing[catalog(CatalogValidationError)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/CatalogValidationError.java
@@ -0,0 +1,16 @@
+/**
+ * @file CatalogValidationError.java
+ * @author Krisna Pranav
+ * @brief Catalog Validation Error
+ * @version 1.0
+ * @date 2025-01-21
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public interface CatalogValidationError {
+    String getErrorDescription();
+}
