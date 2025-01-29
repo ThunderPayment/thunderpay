e7942d33235501a0aaf10f339e5a5a9ec404ee38
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-29 15:04:15 +0530

    sdks: thunderpay-sdk-cs[Extension(EnumerableExt)]

--- sdks/thunderpay-sdk-cs/Extensions/EnumerableExtensions.cs
+++ sdks/thunderpay-sdk-cs/Extensions/EnumerableExtensions.cs
@@ -8,3 +8,33 @@
  * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
  *
  */
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+public static class EnumerableExtensions
+{
+    public static string ToConcatenatedString<T>(this IEnumerable<T> source, Func<T, string> stringSelector)
+    {
+        return source.ToConcatenatedString(stringSelector, String.Empty);
+    } // public static string ToConcatenatedString<T>(this IEnumerable<T> source, Func<T, string> stringSelector)
+
+    public static string ToConcatenatedString<T>(this IEnumerable<T> source, Func<T, string> stringSelector, string separator)
+    {
+        var b = new StringBuilder();
+        bool needsSeparator = false;
+
+        foreach (var item in source)
+        {
+            if (needsSeparator)
+            {
+                b.Append(separator);
+            }
+
+            b.Append(stringSelector(item));
+        }
+
+        return b.ToString();
+    } // public static string ToConcatenatedString<T>(this IEnumerable<T> source, Func<T, string> stringSelector, string separator)
+}
