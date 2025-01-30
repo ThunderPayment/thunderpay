4eb1646ceb0c3dfe41e79001c2f3700599490e1f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-30 18:46:02 +0530

    thunderpay-api: billing[catalog(OverdueActions)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/OverdueActions.java
@@ -0,0 +1,16 @@
+/**
+ * @file OverdueActions.java
+ * @author Krisna Pranav
+ * @brief Overdue Actions
+ * @version 1.0
+ * @date 2025-01-30
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public enum OverdueActions {
+    CANCEL,
+    PAYMENT_RETRY
+}
