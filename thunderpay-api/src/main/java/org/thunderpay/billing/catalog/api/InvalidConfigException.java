fa026023591005fc26b77ae732b15f7fb979bbfd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-30 18:30:07 +0530

    thunderpay-api: billing[catalog(InvalidConfigException)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/InvalidConfigException.java
@@ -0,0 +1,17 @@
+/**
+ * @file InvalidConfigException.java
+ * @author Krisna Pranav
+ * @brief Invalid Config Exception
+ * @version 1.0
+ * @date 2025-01-30
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public class InvalidConfigException extends Exception {
+    
+}
+
