eadfb345ac5b08b46bf2cead4071bede894cecb4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-30 18:49:44 +0530

    thunderpay-api: billing[catalog(PhaseType)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/PhaseType.java
@@ -0,0 +1,18 @@
+/**
+ * @file PhaseType.java
+ * @author Krisna Pranav
+ * @brief Phase Type
+ * @version 1.0
+ * @date 2025-01-30
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public enum PhaseType {
+    TRIAL,
+    DISCOUNT,
+    FIXEDTERM,
+    EVERGREEN
+}
