60503a1bc021e6622b7971330118ce6ffe08c277
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-30 18:51:09 +0530

    thunderpay-api: billing[catalog(PlanAlignmentChange)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/PlanAlignmentChange.java
@@ -0,0 +1,18 @@
+/**
+ * @file PlanAlignmentChange.java
+ * @author Krisna Pranav
+ * @brief Plan Alignment Change
+ * @version 1.0
+ * @date 2025-01-30
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public enum PlanAlignmentChange {
+    START_OF_BUNDLE,
+    START_OF_SUBSCRIPTION,
+    CHANGE_OF_PLAN,
+    CHANGE_OF_PRICELIST
+}
