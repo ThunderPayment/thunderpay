2e7619a400ba97cb17f95162080ef3dc5fe542fc
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-30 18:54:32 +0530

    thunderpay-api: billing[catalog(PlanAlignment)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/PlanAlignmentCreate.java
@@ -0,0 +1,16 @@
+/**
+ * @file PlanAlignmentCreate.java
+ * @author Krisna Pranav
+ * @brief Plan Alignment Create
+ * @version 1.0
+ * @date 2025-01-30
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public enum PlanAlignmentCreate {
+    START_OF_BUNDLE,
+    START_OF_SUBSCRIPTION,
+}
