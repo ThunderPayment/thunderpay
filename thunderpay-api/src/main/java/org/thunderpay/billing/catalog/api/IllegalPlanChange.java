85f5471a62a2dbd54af4477261c66265eebae605
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-30 18:14:37 +0530

    thunderpay-api: billing[catalog(IllegalPlanChange)]

--- thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/IllegalPlanChange.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/IllegalPlanChange.java
@@ -17,6 +17,10 @@ public class IllegalPlanChange extends CatalogApiException {
     private static final long serialVersionUID = 1L;
 
     public IllegalPlanChange(final Throwable cause, final PlanPhaseSpecifier from, final PlanSpecifier to) {
-        super(cause, ErrorCode.CAT_ILLEGAL_CHANGE_REQUEST, from.getProductName())
+        super(cause, ErrorCode.CAT_ILLEGAL_CHANGE_REQUEST, from.getProductName(), from.getBillingPeriod(), from.getPriceListName(), to.getProductName(), to.getBillingPeriod(), to.getPriceListName());
+    }
+
+    public IllegalPlanChange(final PlanPhaseSpecifier from, final PlanSpecifier to) {
+        super(ErrorCode.CAT_ILLEGAL_CHANGE_REQUEST, from.getProductName(), from.getBillingPeriod(), from.getPriceListName(), to.getProductName(), to.getBillingPeriod(), to.getPriceListName());
     }
 }
