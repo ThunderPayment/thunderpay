feed3c31984c24f6c4b57118066d2ab1bab0a1b2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-30 19:03:54 +0530

    thunderpay-api: billing[catalog(PlanPhasePriceOver)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/PlanPhasePriceOverride.java
@@ -0,0 +1,24 @@
+/**
+ * @file PlanPhasePriceOverride.java
+ * @author Krisna Pranav
+ * @brief Plan Phase Price Override
+ * @version 1.0
+ * @date 2025-01-30
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface PlanPhasePriceOverride {
+    public String getPhaseName();
+
+    public PlanPhaseSpecifier getPlanPhaseSpecifier();
+
+    public Currency getCurrency();
+
+    public BigDecimal getFixedPrice();
+}
