f3288a10f06517d342c672249f59b6808344de3f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-30 18:11:28 +0530

    thunderpay-api: billing[catalog(FixedType)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/FixedType.java
@@ -0,0 +1,16 @@
+/**
+ * @file FixedType.java
+ * @author Krisna Pranav
+ * @brief Fixed Type
+ * @version 1.0
+ * @date 2025-01-21
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public enum FixedType {
+    ONE_TIME,
+}
