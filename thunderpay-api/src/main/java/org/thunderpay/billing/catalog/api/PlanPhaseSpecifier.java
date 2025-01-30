138b65cc31a2412c4a37335b0d90033bfedb45d8
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-30 19:10:43 +0530

    thunderpay-api: billing[catalog(PlanPhaseSpecifier)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/PlanPhaseSpecifier.java
@@ -0,0 +1,28 @@
+/**
+ * @file PlanPhaseSpecifier.java
+ * @author Krisna Pranav
+ * @brief Plan Phase Specifier
+ * @version 1.0
+ * @date 2025-01-30
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ */
+
+package org.thunderpay.billing.catalog.api;
+
+public class PlanPhaseSpecifier extends PlanSpecifier {
+    private final PhaseType phaseType;
+
+    public PlanPhaseSpecifier(final String productName, final BillingPeriod billingPeriod, final String priceListName) {
+        this(productName, billingPeriod, priceListName, null);
+    }
+
+    public PlanPhaseSpecifier(final String productName, final BillingPeriod billingPeriod, final String priceListName, final PhaseType phaseType)  {
+        super(productName, billingPeriod, priceListName);
+        this.phaseType = phaseType;
+    }
+    
+    public PhaseType getPhaseType() {
+        return phaseType;
+    }
+}
