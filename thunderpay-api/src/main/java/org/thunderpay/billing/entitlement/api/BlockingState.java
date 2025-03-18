2b570400994612afb9066e7fabc2d8580189116f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-18 18:14:48 +0530

    thunderpay-api: billing[entitlement(api - BlockingState)]

--- thunderpay-api/src/main/java/org/thunderpay/billing/entitlement/api/BlockingState.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/entitlement/api/BlockingState.java
@@ -5,6 +5,7 @@ import org.joda.time.DateTime;
 import org.thunderpay.billing.util.entity.Entity;
 
 public interface BlockingState extends Entity, Comparable<BlockingState> {
+
     public UUID getBlockedId();
 
     public String getStateName();
@@ -12,4 +13,22 @@ public interface BlockingState extends Entity, Comparable<BlockingState> {
     public BlockingStateType getType();
 
     public DateTime getEffectiveDate();
+
+    public boolean isBlockChange();
+
+    public boolean isBlockEntitlement();
+
+    public boolean isBlockBilling();
+
+    public int compareTo(BlockingState arg0);
+
+    public int hashCode();
+
+    public String getDescription();
+
+    public String toString();
+
+    public String getService();
+
+    public boolean isActive();
 }
