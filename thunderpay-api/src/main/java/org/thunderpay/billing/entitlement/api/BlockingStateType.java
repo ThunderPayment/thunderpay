33bb347925b2d3c8cb90331204080402808b7646
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-18 18:38:41 +0530

    thunderpay-api: billing[entitlement(api - BlockingStateType)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/entitlement/api/BlockingStateType.java
@@ -0,0 +1,17 @@
+/**
+ * @file BlockingStateType
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-01-19
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.entitlement.api;
+
+public enum BlockingStateType {
+    SUBSCRIPTION,
+    SUBSCRIPTION_BUNDLE,
+    ACCOUNT
+}
