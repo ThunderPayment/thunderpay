ed83ed8860e7d9fcd834fd39d77e3a494915e993
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-18 20:27:58 +0530

    thunderpay-api: billing[api(ColumnInfo)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/util/api/ColumnInfo.java
@@ -0,0 +1,10 @@
+package org.thunderpay.billing.util.api;
+
+public interface ColumnInfo {
+
+    public String getTableName();
+
+    public String getColumnName();
+
+    public String getDataType();
+}
