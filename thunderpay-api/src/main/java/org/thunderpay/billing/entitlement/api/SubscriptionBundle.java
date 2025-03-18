cc91689b8df13eefa318d2b309da844c3ed7bb97
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-18 19:38:02 +0530

    thunderpay-api: billing[entitlement(api - SubscriptionBundle)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/entitlement/api/SubscriptionBundle.java
@@ -0,0 +1,30 @@
+/**
+ * @file SubscriptionBundle.java
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-01-19
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.entitlement.api;
+
+
+import java.util.List;
+import java.util.UUID;
+import org.joda.time.DateTime;
+import org.thunderpay.billing.util.entity.Entity;
+
+public interface SubscriptionBundle extends Entity {
+    public UUID getAccountId();
+
+    public String getExternalKey();
+
+    public DateTime getOriginalCreatedDate();
+
+    public List<Subscription> getSubscriptions();
+    
+    public SubscriptionBundleTimeline getTimeline();
+
+}
