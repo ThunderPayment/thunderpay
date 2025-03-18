52f2c1622578f9cc1c366634d71b1c397807befe
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-18 13:48:49 +0530

    thunderpay-api: billing[catalog(Plan)]

--- thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/Plan.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/catalog/api/Plan.java
@@ -8,23 +8,52 @@
  * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
  */
 
-
 package org.thunderpay.billing.catalog.api;
 
 import java.util.Date;
 import java.util.Iterator;
+
 import org.joda.time.DateTime;
 
 public interface Plan extends CatalogEntity {
+
+    public StaticCatalog getCatalog();
+
+    /**
+     * @return the recurring billing mode for this catalog
+     */
+    public BillingMode getRecurringBillingMode();
+
+    /**
+     * @return an array of {@code PlanPhase}
+     */
+    public PlanPhase[] getInitialPhases();
+
+    /**
+     * @return the {@code Product} associated with that {@code Plan}
+     */
+    public Product getProduct();
+
+    /**
+     *
+     * @return the {@PriceList} in which that {@code Plan} is defined
+     */
     public PriceList getPriceList();
 
     public Iterator<PlanPhase> getInitialPhaseIterator();
 
     public PlanPhase getFinalPhase();
-    
+
+    public BillingPeriod getRecurringBillingPeriod();
+
     public int getPlansAllowedInBundle();
+
+    public PlanPhase[] getAllPhases();
+
+    public Date getEffectiveDateForExistingSubscriptions();
 
     public PlanPhase findPhase(String name) throws CatalogApiException;
 
-    public DateTime dateOfFirstRecurringNonZeroCharge(DateTime subscriptionStartDate, PhaseType intiialPhase);
+    public DateTime dateOfFirstRecurringNonZeroCharge(DateTime subscriptionStartDate, PhaseType intialPhaseType);
+
 }
