ee8ff5b463c3d31e59e144194799cecec4d6689e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-18 19:43:23 +0530

    thunderpay-api: billing[usage(api - RawUsageRecord)]

--- thunderpay-api/src/main/java/org/thunderpay/billing/usage/api/RawUsageRecord.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/usage/api/RawUsageRecord.java
@@ -1,3 +1,14 @@
+/**
+ * @file RawUsageRecord.java
+ * @author Krisna Pranav
+ * @brief RAw Usage Recorder
+ * @version 1.0
+ * @date 2025-03-18
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
 package org.thunderpay.billing.usage.api;
 
 import java.math.BigDecimal;
