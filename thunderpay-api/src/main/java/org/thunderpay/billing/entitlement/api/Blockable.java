963e853280c59b38dbabe99683496322bd7f2fea
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-18 18:11:56 +0530

    thunderpay-api: billing[entitlement(Blockable)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/entitlement/api/Blockable.java
@@ -0,0 +1,17 @@
+/**
+ * @file Blockable
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-01-19
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.entitlement.api;
+
+import org.thunderpay.billing.util.entity.Entity;
+
+public interface Blockable extends Entity {
+
+}
