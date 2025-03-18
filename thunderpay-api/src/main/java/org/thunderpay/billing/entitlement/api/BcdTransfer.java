601b9df356e4b461b9dabf1acce84dad7ba1fe77
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-18 18:10:37 +0530

    thunderpay-api: billing[entitlement(BcdTransfer)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/entitlement/api/BcdTransfer.java
@@ -0,0 +1,17 @@
+/**
+ * @file BcdTransfer
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-01-19
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.entitlement.api;
+
+public enum BcdTransfer {
+    NONE,
+    USE_EXISTING,
+    ALIGN_WITH_EFF_DT,
+}
