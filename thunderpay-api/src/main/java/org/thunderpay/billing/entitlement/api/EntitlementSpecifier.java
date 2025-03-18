3915f857002c185f89f7022a778fe8335fcc261c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-18 14:10:46 +0530

    thunderpay-api: billing[entitlement - EntitlementSpecifier]

--- thunderpay-api/src/main/java/org/thunderpay/billing/entitlement/api/EntitlementSpecifier.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/entitlement/api/EntitlementSpecifier.java
@@ -1,4 +1,28 @@
+/**
+ * @file EntitlementSpecifier.java
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-01-19
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
 package org.thunderpay.billing.entitlement.api;
 
-public class EntitlementSpecifier {
+import java.util.List;
+import org.thunderpay.billing.catalog.api.PlanPhasePriceOverride;
+import org.thunderpay.billing.catalog.api.PlanPhaseSpecifier;
+
+public interface EntitlementSpecifier {
+    PlanPhaseSpecifier getPlanPhaseSpecifier();
+
+    Integer getBillCycleDay();
+
+    Integer getQuantity();
+
+    String getExternalKey();
+
+    List<PlanPhasePriceOverride> getOverrides();
+
 }
