057c6802c00c8416af934cc96de8096bc6447ec6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-18 18:46:55 +0530

    thunderpay-api: billing[entitlement(api - EntitlementAOStatusDryRun)]

--- thunderpay-api/src/main/java/org/thunderpay/billing/entitlement/api/EntitlementAOStatusDryRun.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/entitlement/api/EntitlementAOStatusDryRun.java
@@ -10,17 +10,27 @@
 
 package org.thunderpay.billing.entitlement.api;
 
+import java.util.UUID;
+import org.thunderpay.billing.catalog.api.BillingPeriod;
 import org.thunderpay.billing.catalog.api.PhaseType;
 
-import java.util.UUID;
-
 public interface EntitlementAOStatusDryRun {
+
     public UUID getId();
+
+    public String getProductName();
+
+    public BillingPeriod getBillingPeriod();
+
+    public String getPriceList();
 
     public PhaseType getPhaseType();
 
+    public DryRunChangeReason getReason();
+
     public enum DryRunChangeReason {
         AO_INCLUDED_IN_NEW_PLAN,
-        AO_NOT_AVAILABLE_IN_NEW_PLAN
+        AO_NOT_AVAILABLE_IN_NEW_PLAN,
+        AO_AVAILABLE_IN_NEW_PLAN
     }
 }
