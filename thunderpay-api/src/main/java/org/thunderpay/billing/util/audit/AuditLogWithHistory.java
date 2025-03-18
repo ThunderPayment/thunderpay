16ddd331e59cce9978b63b45f8bca78b797092d1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-18 20:32:42 +0530

    thunderpay-api: billing[audit(AuditLogWithHistory)]

--- thunderpay-api/src/main/java/org/thunderpay/billing/util/audit/AuditLogWithHistory.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/util/audit/AuditLogWithHistory.java
@@ -1,3 +1,14 @@
+/**
+ * @file AuditLogWithHistory.java
+ * @author Krisna Pranav
+ * @brief Audit Log With History
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
 package org.thunderpay.billing.util.audit;
 
 import org.thunderpay.billing.util.entity.Entity;
