fa8682bedbd698b48250a1929d6bd6662e9b532a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-18 20:26:52 +0530

    thunderpay-api: billing[audit(ChangeType)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/util/audit/ChangeType.java
@@ -0,0 +1,18 @@
+/**
+ * @file ChangeType.java
+ * @author Krisna Pranav
+ * @brief Change Type
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.util.audit;
+
+public enum ChangeType {
+    INSERT,
+    UPDATE,
+    DELETE
+}
