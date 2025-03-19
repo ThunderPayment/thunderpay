5dc6dd2f2037391ee4275d399d17461c098ce79e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-19 16:34:32 +0530

    thunderpay-api: billing[osgi(api - OSGIPluginProperties)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/osgi/api/OSGIPluginProperties.java
@@ -0,0 +1,17 @@
+/**
+ * @file OSGIPluginProperties.java
+ * @author Krisna Pranav
+ * @brief OSGI Plugin Props
+ * @version 1.0
+ * @date 2025-03-19
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.osgi.api;
+
+public interface OSGIPluginProperties {
+    public static final String PLUGIN_NAME_PROP = "thunderpay.pluginName";
+
+}
