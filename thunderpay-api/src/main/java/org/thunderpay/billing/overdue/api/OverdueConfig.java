0c924eb6b5a5b56b2ef74fb85a91b50cdcabfbec
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-19 16:40:24 +0530

    thunderpay-api: billing[overdue(api - OverdueConfig)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/overdue/api/OverdueConfig.java
@@ -0,0 +1,16 @@
+/**
+ * @file OverdueConfig.java
+ * @author Krisna Pranav
+ * @brief Overdue Config
+ * @version 1.0
+ * @date 2025-03-19
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.overdue.api;
+
+public interface OverdueConfig {
+    public OverdueStatesAccount getOverdueStatesAccount();
+}
