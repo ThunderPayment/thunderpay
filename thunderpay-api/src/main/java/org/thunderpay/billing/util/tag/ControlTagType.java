96691fa0d6aa2fdacda3cf55d6f8e37c3b940410
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-19 16:04:39 +0530

    thunderpay-api: billing[util(ControlTagType)]

--- thunderpay-api/src/main/java/org/thunderpay/billing/util/tag/ControlTagType.java
+++ thunderpay-api/src/main/java/org/thunderpay/billing/util/tag/ControlTagType.java
@@ -3,7 +3,7 @@
  * @author Krisna Pranav
  * @brief Control Tag Type
  * @version 1.0
- * @date 2024-11-25
+ * @date 2024-03-19
  *
  * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
  *
