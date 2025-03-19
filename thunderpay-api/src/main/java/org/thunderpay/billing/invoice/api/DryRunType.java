6abaa24577f54948423f1c12ceba765f4b3ad552
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-19 16:05:24 +0530

    thunderpay-api: billing[api(DryRunType)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/invoice/api/DryRunType.java
@@ -0,0 +1,18 @@
+/**
+ * @file DryRunType.java
+ * @author Krisna Pranav
+ * @brief Dry Run Type
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.invoice.api;
+
+public enum DryRunType {
+    TARGET_DATE,
+    UPCOMING_INVOICE,
+    SUBSCRIPTION_ACTION
+}
