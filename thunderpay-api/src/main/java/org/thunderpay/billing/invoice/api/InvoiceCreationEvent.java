8e28f6672c4523c15c863e6f7b906a895ed73267
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-19 16:07:49 +0530

    thunderpay-api: billing[invoice(InvoiceCreationEvent)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/invoice/api/InvoiceCreationEvent.java
@@ -0,0 +1,16 @@
+/**
+ * @file InvoiceCreationEvent.java
+ * @author Krisna Pranav
+ * @brief Invoice Creation Event
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.invoice.api;
+
+public interface InvoiceCreationEvent {
+
+}
