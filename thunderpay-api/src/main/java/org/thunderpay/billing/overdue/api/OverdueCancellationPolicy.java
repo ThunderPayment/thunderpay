4cb4ac970551e8ce7e50344d6d2c787a2660279a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-19 16:38:59 +0530

    thunderpay-api: billing[overdue(api - OverdueCancellationPolicy)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/overdue/api/OverdueCancellationPolicy.java
@@ -0,0 +1,18 @@
+/**
+ * @file OverdueCancellationPolicy.java
+ * @author Krisna Pranav
+ * @brief Overdue Cancellation Policy
+ * @version 1.0
+ * @date 2025-03-19
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.overdue.api;
+
+public enum OverdueCancellationPolicy {
+    END_OF_TERM,
+    IMMEDIATE,
+    NONE
+}
