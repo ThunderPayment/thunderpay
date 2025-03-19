aea50d75f5a48f86d4b03580afe52d6d637668f5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-19 16:13:54 +0530

    thunderpay-api: billing[invoice(InvoicePaymentStatus)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/invoice/api/InvoicePaymentStatus.java
@@ -0,0 +1,18 @@
+/**
+ * @file InvoicePaymentStatus.java
+ * @author Krisna Pranav
+ * @brief Invoice Payment Status
+ * @version 1.0
+ * @date 2024-11-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.invoice.api;
+
+public enum InvoicePaymentStatus {
+    INIT,
+    PENDING,
+    SUCCESS,
+}
