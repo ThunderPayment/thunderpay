c570197623d9f54d1fea5a6726238505db0f5ab6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-22 18:14:13 +0530

    thunderpay-lib: utils[VisibleForTesting]

---
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/utils/annotation/VisibleForTesting.java
@@ -0,0 +1,16 @@
+/**
+ * @file VisibleForTesting.java
+ * @author Krisna Pranav
+ * @brief Visible For Testing
+ * @version 1.0
+ * @date 2025-03-22
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+
+package org.thunderpay.commons.utils.annotation;
+
+public @interface VisibleForTesting {
+}
