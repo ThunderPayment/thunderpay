8c9489850d6f8351add699d555db1c75bed0c1d4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-22 18:49:05 +0530

    thunderpay-lib: utils[math(Ints)]

---
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/utils/math/Ints.java
@@ -0,0 +1,24 @@
+/**
+ * @file Ints.java
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-03-22
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+
+package org.thunderpay.commons.utils.math;
+
+public final class Ints {
+    public static int saturatedCast(final long value) {
+        if (value > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        if (value < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+        return (int) value;
+    }
+}
