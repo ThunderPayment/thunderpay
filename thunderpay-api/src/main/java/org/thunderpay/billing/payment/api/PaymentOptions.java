ca39c3760de9f2d5e35727dcaae08da0eb284850
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-22 13:12:09 +0530

    thunderpay-api: billing[payment(api - PaymentOptions)]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/payment/api/PaymentOptions.java
@@ -0,0 +1,20 @@
+/**
+ * @file PaymentOptions.java
+ * @author Krisna Pranav
+ * @brief Payment Options
+ * @version 1.0
+ * @date 2025-03-19
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.payment.api;
+
+import java.util.List;
+
+public interface PaymentOptions {
+    public boolean isExternalPayment();
+    
+    public List<String> getPaymentControlPluginNames();
+}
