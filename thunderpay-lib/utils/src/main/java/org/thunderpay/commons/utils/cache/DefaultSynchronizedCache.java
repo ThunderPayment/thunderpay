cb88affc7dd83b21abb9f8de803a77a11dc39607
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-22 18:16:07 +0530

    thunderpay-lib: utils[cache(DefaultSynchronizedCache)]

---
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/utils/cache/DefaultSynchronizedCache.java
@@ -0,0 +1,57 @@
+/**
+ * @file DefaultSyncCache.java
+ * @author Krisna Pranav
+ * @brief Default Sync Cache
+ * @version 1.0
+ * @date 2025-03-22
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.commons.utils.cache;
+
+import java.util.function.Function;
+
+public class DefaultSynchronizedCache<K, V> extends DefaultCache<K, V> implements Cache<K, V> {
+
+    public DefaultSynchronizedCache(final int maxSize) {
+        super(maxSize);
+    }
+
+    public DefaultSynchronizedCache(final Function<K, V> cacheLoader) {
+        super(cacheLoader);
+    }
+
+    public DefaultSynchronizedCache(final int maxSize, final long timeoutInSecond, final Function<K, V> cacheLoader) {
+        super(maxSize, timeoutInSecond, cacheLoader);
+    }
+
+    @Override
+    public V get(final K key) {
+        synchronized (this) {
+            return super.get(key);
+        }
+    }
+
+    @Override
+    public V getOrLoad(final K key, final Function<K, V> loader) {
+        synchronized (this) {
+            return super.getOrLoad(key, loader);
+        }
+    }
+
+    @Override
+    public void put(final K key, final V value) {
+        synchronized (this) {
+            super.put(key, value);
+        }
+    }
+
+    @Override
+    public void invalidate(final K key) {
+        synchronized (this) {
+            super.invalidate(key);
+        }
+    }
+}
