11e587c8339602655832eedd2a5d25deacf5b58e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-22 17:03:12 +0530

    thunderpay-lib: utils[eventbus(AllowConcurrentEvents)]

---
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/eventbus/AllowConcurrentEvents.java
@@ -0,0 +1,21 @@
+/**
+ * @file AllowConcurrentEvents.java
+ * @author Krisna Pranav
+ * @brief Allow Concurrent Events
+ * @version 1.0
+ * @date 2025-03-22
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.commons.eventbus;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface AllowConcurrentEvents {}
