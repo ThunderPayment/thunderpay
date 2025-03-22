f5407ea2d68fe9765de787d59b406a92132a8852
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-22 18:03:36 +0530

    thunderpay-lib: utils[eventbus(package-info)]

---
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/eventbus/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @file package-info.java
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-03-22
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.commons.eventbus;
