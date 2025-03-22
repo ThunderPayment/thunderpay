8b85efbe3fcdbb7cd0a9a48dd93719af5595c459
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-22 17:16:13 +0530

    thunderpay-lib: utils[eventbus(Subscribe)]

---
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/eventbus/Subscribe.java
@@ -0,0 +1,21 @@
+/**
+ * @file Subscriber.java
+ * @author Krisna Pranav
+ * @brief Subscribe
+ * @version 1.0
+ * @date 2025-03-22
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.commons.eventbus;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Subscribe {}
