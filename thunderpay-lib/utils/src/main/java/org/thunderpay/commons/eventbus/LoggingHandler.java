f787a80c36a33c6a5a49bbee45b280458a0e5595
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-22 17:14:36 +0530

    thunderpay-lib: utils[eventbus(LoggingHandler)]

---
+++ thunderpay-lib/utils/src/main/java/org/thunderpay/commons/eventbus/LoggingHandler.java
@@ -0,0 +1,46 @@
+/**
+ * @file LoggingHandler.java
+ * @author Krisna Pranav
+ * @brief Logging Handler
+ * @version 1.0
+ * @date 2025-03-22
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.commons.eventbus;
+
+import java.lang.reflect.Method;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class LoggingHandler implements SubscriberExceptionHandler {
+
+    static final SubscriberExceptionHandler INSTANCE = new LoggingHandler();
+
+    private static Logger logger(final SubscriberExceptionContext context) {
+        return Logger.getLogger(EventBus.class.getName() + "." + context.getEventBus().identifier());
+    }
+
+    private static String message(final SubscriberExceptionContext context) {
+        final Method method = context.getSubscriberMethod();
+        return "Exception thrown by subscriber method "
+                + method.getName()
+                + '('
+                + method.getParameterTypes()[0].getName()
+                + ')'
+                + " on subscriber "
+                + context.getSubscriber()
+                + " when dispatching event: "
+                + context.getEvent();
+    }
+
+    @Override
+    public void handleException(final Throwable exception, final SubscriberExceptionContext context) {
+        final Logger logger = logger(context);
+        if (logger.isLoggable(Level.SEVERE)) {
+            logger.log(Level.SEVERE, message(context), exception);
+        }
+    }
+}
