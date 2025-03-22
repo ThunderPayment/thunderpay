270e17b94284baeaae7f772c27b2166b3575d83c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-22 16:16:05 +0530

    thunderpay-api: billing[plugin - GatewayNotification]

---
+++ thunderpay-api/src/main/java/org/thunderpay/billing/payment/plugin/api/GatewayNotification.java
@@ -0,0 +1,29 @@
+/**
+ * @file GatewayNotification.java
+ * @author Krisna Pranav
+ * @brief Gateway Notifications
+ * @version 1.0
+ * @date 2025-03-22
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.billing.payment.plugin.api;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import org.thunderpay.billing.payment.api.PluginProperty;
+
+public interface GatewayNotification {
+    public UUID getKbPaymentId();
+
+    public int getStatus();
+
+    public String getEntity();
+
+    public Map<String, List<String>> getHeaders();
+    
+    public List<PluginProperty> getProperties();
+}
