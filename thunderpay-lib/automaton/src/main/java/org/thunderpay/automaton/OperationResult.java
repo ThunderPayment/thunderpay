146f0142d72770cc4ce79f89a9b8302b57424410
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-23 17:16:59 +0530

    thunderpay-lib: automaton[OperationResult]

---
+++ thunderpay-lib/automaton/src/main/java/org/thunderpay/automaton/OperationResult.java
@@ -0,0 +1,18 @@
+/**
+ * @file OperationResult.java
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-03-22
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.automaton;
+
+public enum OperationResult {
+    PENDING,
+    SUCCESS,
+    FAILURE,
+    EXCEPTION
+}
