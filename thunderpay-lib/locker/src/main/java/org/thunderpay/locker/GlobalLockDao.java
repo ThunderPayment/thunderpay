d55ad1f22fabe44147e854f161e7dfb43d7614a4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-25 18:48:42 +0530

    thunderpay-lib: locker[GlobalLockDao]

---
+++ thunderpay-lib/locker/src/main/java/org/thunderpay/locker/GlobalLockDao.java
@@ -0,0 +1,15 @@
+/**
+ * @file GlobalLockDao.java
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-03-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.locker;
+
+public class GlobalLockDao {
+    
+}
