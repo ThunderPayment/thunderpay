64d2b4e26b4cacfa0d85648e9d17b3ede8174eae
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-25 18:23:13 +0530

    thunderpay-lib: locker[GlobalLock]

---
+++ thunderpay-lib/locker/src/main/java/org/thunderpay/locker/GlobalLock.java
@@ -0,0 +1,15 @@
+/**
+ * @file GlobalLock.java
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-03-25
+ *
+ * @copyright Copyright (c) 2024 ThunderPayment Developers, Krisna Pranav
+ *
+ */
+
+package org.thunderpay.locker;
+
+public interface GlobalLock {
+    void release();
+}
